847808f024d7d5245d700f3491d1adfbb15bcce9
mixichao <mixichao@jd.com>
2020-06-10 15:02:39 +0800

    [Fix] XId.GetTransactionId返回总是0

--- base/common/xid.go
+++ base/common/xid.go
@@ -7,20 +7,25 @@ import (
 )
 
 type XId struct {
-	Port int
+	Port      int
 	IpAddress string
 }
 
 var XID = &XId{}
 
 func (xId *XId) GenerateXID(tranId int64) string {
-	return fmt.Sprintf("%s:%d:%d",xId.IpAddress,xId.Port,tranId)
+	return fmt.Sprintf("%s:%d:%d", xId.IpAddress, xId.Port, tranId)
 }
 
 func (xId *XId) GetTransactionId(xid string) int64 {
-	if xid == "" { return -1 }
+	if xid == "" {
+		return -1
+	}
 
-	idx := strings.LastIndex(xid,":")
-	tranId,_ := strconv.ParseInt(xid[idx:],10,64)
+	idx := strings.LastIndex(xid, ":")
+	if len(xid) == idx+1 {
+		return -1
+	}
+	tranId, _ := strconv.ParseInt(xid[idx+1:], 10, 64)
 	return tranId
 }
