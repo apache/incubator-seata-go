e946c5dda4b8274d3df02fd6e10d06f0049904f1
baoyazhang <baoyazhang@dalongyun.com>
2020-07-28 18:03:35 +0800

    seata-golang/client/config添加 appllo配置:config.InitEtcdConf(172.20.0.11:8080, seata, seata.yml)

--- client/config/client_config.go
+++ client/config/client_config.go
@@ -4,20 +4,20 @@ import (
 	"fmt"
 	"io/ioutil"
 	"path"
-)
 
-import (
 	"github.com/pkg/errors"
+	"github.com/shima-park/agollo"
+	"github.com/xiaobudongzhang/seata-golang/client/at/sql/struct/cache"
 	"gopkg.in/yaml.v2"
 )
 
 type ClientConfig struct {
-	ApplicationId		    string `yaml:"application_id" json:"application_id,omitempty"`
-	TransactionServiceGroup string `yaml:"transaction_service_group" json:"transaction_service_group,omitempty"`
-	SeataVersion            string `yaml:"seata_version" json:"seata_version,omitempty"`
+	ApplicationId           string      `yaml:"application_id" json:"application_id,omitempty"`
+	TransactionServiceGroup string      `yaml:"transaction_service_group" json:"transaction_service_group,omitempty"`
+	SeataVersion            string      `yaml:"seata_version" json:"seata_version,omitempty"`
 	GettyConfig             GettyConfig `yaml:"getty" json:"getty,omitempty"`
-	TMConfig                TMConfig `yaml:"tm" json:"tm,omitempty"`
-	ATConfig                ATConfig `yaml:"at" json:"at,omitempty"`
+	TMConfig                TMConfig    `yaml:"tm" json:"tm,omitempty"`
+	ATConfig                ATConfig    `yaml:"at" json:"at,omitempty"`
 }
 
 var clientConfig ClientConfig
@@ -36,25 +36,24 @@ func GetDefaultClientConfig(applicationId string) ClientConfig {
 	}
 }
 
-
 func InitConf(confFile string) error {
 	var err error
 
 	if confFile == "" {
-		return errors.WithMessagef(err,fmt.Sprintf("application configure file name is nil"))
+		return errors.WithMessagef(err, fmt.Sprintf("application configure file name is nil"))
 	}
 	if path.Ext(confFile) != ".yml" {
-		return errors.WithMessagef(err,fmt.Sprintf("application configure file name{%v} suffix must be .yml", confFile))
+		return errors.WithMessagef(err, fmt.Sprintf("application configure file name{%v} suffix must be .yml", confFile))
 	}
 
 	clientConfig = ClientConfig{}
 	confFileStream, err := ioutil.ReadFile(confFile)
 	if err != nil {
-		return errors.WithMessagef(err,fmt.Sprintf("ioutil.ReadFile(file:%s) = error:%s", confFile, err))
+		return errors.WithMessagef(err, fmt.Sprintf("ioutil.ReadFile(file:%s) = error:%s", confFile, err))
 	}
 	err = yaml.Unmarshal(confFileStream, &clientConfig)
 	if err != nil {
-		return errors.WithMessagef(err,fmt.Sprintf("yaml.Unmarshal() = error:%s", err))
+		return errors.WithMessagef(err, fmt.Sprintf("yaml.Unmarshal() = error:%s", err))
 	}
 
 	(&clientConfig).GettyConfig.CheckValidity()
@@ -67,4 +66,31 @@ func InitConfWithDefault(applicationId string) {
 	clientConfig = GetDefaultClientConfig(applicationId)
 	(&clientConfig).GettyConfig.CheckValidity()
 	tmConfig = clientConfig.TMConfig
+}
+
+func InitEtcdConf(serverAddr string, appId string, nameSpace string) error {
+
+	a, err := agollo.New(serverAddr, appId, agollo.AutoFetchOnCacheMiss())
+	if err != nil {
+		return errors.WithMessagef(err, fmt.Sprintf("get etcd error:%s", err))
+	}
+
+	var config = a.Get("content", agollo.WithNamespace(nameSpace))
+	return initCommonConf([]byte(config))
+}
+func initCommonConf(confStream []byte) error {
+	var err error
+	err = yaml.Unmarshal(confStream, &clientConfig)
+	fmt.Println("config", clientConfig)
+	if err != nil {
+		return errors.WithMessagef(err, fmt.Sprintf("yaml.Unmarshal() = error:%s", err))
+	}
+
+	(&clientConfig).GettyConfig.CheckValidity()
+	(&clientConfig).ATConfig.CheckValidity()
+
+	if clientConfig.ATConfig.DSN != "" {
+		cache.SetTableMetaCache(cache.NewMysqlTableMetaCache(clientConfig.ATConfig.DSN))
+	}
+	return nil
 }
