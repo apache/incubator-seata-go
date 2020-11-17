e4fe22e66035eeb702fffd2e9449f0e5e96dbd02
liuxiaomin <dugukuangshao@qq.com>
2020-11-17 22:10:17 +0800

    update log helper

--- go.mod
+++ go.mod
@@ -31,6 +31,7 @@ require (
 	github.com/stretchr/testify v1.6.1
 	github.com/urfave/cli/v2 v2.2.0
 	go.uber.org/atomic v1.6.0
+	go.uber.org/zap v1.15.0
 	golang.org/x/crypto v0.0.0-20191206172530-e9b2fee46413 // indirect
 	golang.org/x/lint v0.0.0-20200302205851-738671d3881b // indirect
 	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd // indirect
--- pkg/util/log/logging.go
+++ pkg/util/log/logging.go
@@ -1,35 +1,168 @@
 package log
 
-import getty "github.com/apache/dubbo-getty"
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
 
+// Level represents the level of logging.
+type LogLevel int8
+
+const (
+	// DebugLevel logs are typically voluminous, and are usually disabled in
+	// production.
+	DebugLevel = LogLevel(zapcore.DebugLevel)
+	// InfoLevel is the default logging priority.
+	InfoLevel = LogLevel(zapcore.InfoLevel)
+	// WarnLevel logs are more important than Info, but don't need individual
+	// human review.
+	WarnLevel = LogLevel(zapcore.WarnLevel)
+	// ErrorLevel logs are high-priority. If an application is running smoothly,
+	// it shouldn't generate any error-level logs.
+	ErrorLevel = LogLevel(zapcore.ErrorLevel)
+	// PanicLevel logs a message, then panics.
+	PanicLevel = LogLevel(zapcore.PanicLevel)
+	// FatalLevel logs a message, then calls os.Exit(1).
+	FatalLevel = LogLevel(zapcore.FatalLevel)
+)
+
+type Logger interface {
+	Debug(v ...interface{})
+	Debugf(format string, v ...interface{})
+
+	Info(v ...interface{})
+	Infof(format string, v ...interface{})
+
+	Warn(v ...interface{})
+	Warnf(format string, v ...interface{})
+
+	Error(v ...interface{})
+	Errorf(format string, v ...interface{})
+
+	Panic(v ...interface{})
+	Panicf(format string, v ...interface{})
+
+	Fatal(v ...interface{})
+	Fatalf(format string, v ...interface{})
+}
+
+var (
+	log       Logger
+	zapLogger *zap.Logger
+
+	zapLoggerConfig        = zap.NewDevelopmentConfig()
+	zapLoggerEncoderConfig = zapcore.EncoderConfig{
+		TimeKey:        "time",
+		LevelKey:       "level",
+		NameKey:        "logger",
+		CallerKey:      "caller",
+		MessageKey:     "message",
+		StacktraceKey:  "stacktrace",
+		EncodeLevel:    zapcore.CapitalColorLevelEncoder,
+		EncodeTime:     zapcore.ISO8601TimeEncoder,
+		EncodeDuration: zapcore.SecondsDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder,
+	}
+)
+
+func init() {
+	zapLoggerConfig.EncoderConfig = zapLoggerEncoderConfig
+	zapLogger, _ = zapLoggerConfig.Build()
+	log = zapLogger.Sugar()
+}
+
+// SetLogger: customize yourself logger.
+func SetLogger(logger Logger) {
+	log = logger
+}
+
+// GetLogger get logger
+func GetLogger() Logger {
+	return log
+}
+
+// SetLoggerLevel
+func SetLoggerLevel(level LogLevel) error {
+	var err error
+	zapLoggerConfig.Level = zap.NewAtomicLevelAt(zapcore.Level(level))
+	zapLogger, err = zapLoggerConfig.Build()
+	if err != nil {
+		return err
+	}
+	log = zapLogger.Sugar()
+	return nil
+}
+
+// SetLoggerCallerDisable: disable caller info in production env for performance improve.
+// It is highly recommended that you execute this method in a production environment.
+func SetLoggerCallerDisable() error {
+	var err error
+	zapLoggerConfig.Development = false
+	zapLoggerConfig.DisableCaller = true
+	zapLogger, err = zapLoggerConfig.Build()
+	if err != nil {
+		return err
+	}
+	log = zapLogger.Sugar()
+	return nil
+}
+
+// Debug ...
 func Debug(v ...interface{}) {
-	getty.GetLogger().Debug(v)
+	log.Debug(v)
 }
 
+// Debugf ...
 func Debugf(format string, v ...interface{}) {
-	getty.GetLogger().Debugf(format, v)
+	log.Debugf(format, v)
 }
 
+// Info ...
 func Info(v ...interface{}) {
-	getty.GetLogger().Info(v)
+	log.Info(v)
 }
 
+// Infof ...
 func Infof(format string, v ...interface{}) {
-	getty.GetLogger().Infof(format, v)
+	log.Infof(format, v)
 }
 
+// Warn ...
 func Warn(v ...interface{}) {
-	getty.GetLogger().Warn(v)
+	log.Warn(v)
 }
 
+// Warnf ...
 func Warnf(format string, v ...interface{}) {
-	getty.GetLogger().Warnf(format, v)
+	log.Warnf(format, v)
 }
 
+// Error ...
 func Error(v ...interface{}) {
-	getty.GetLogger().Error(v)
+	log.Error(v)
 }
 
+// Errorf ...
 func Errorf(format string, v ...interface{}) {
-	getty.GetLogger().Errorf(format, v)
+	log.Errorf(format, v)
+}
+
+// Panic ...
+func Panic(v ...interface{}) {
+	log.Panic(v)
+}
+
+// Panicf ...
+func Panicf(format string, v ...interface{}) {
+	log.Panicf(format, v)
+}
+
+// Fatal ...
+func Fatal(v ...interface{}) {
+	log.Fatal(v)
+}
+
+// Fatalf ...
+func Fatalf(format string, v ...interface{}) {
+	log.Fatalf(format, v)
 }
