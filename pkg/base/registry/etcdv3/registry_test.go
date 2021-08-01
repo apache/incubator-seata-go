c24beec9221d85c400f994120d354f094b8ce061
MasterKenway <zk@ikenway.net>
2021-08-01 17:15:20 +0800

    feat: uni test

--- pkg/base/registry/etcdv3/registry_test.go
+++ pkg/base/registry/etcdv3/registry_test.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"sync"
 	"testing"
+	"time"
 )
 
 import (
@@ -28,6 +29,7 @@ func initRegistry(ctx context.Context, t *testing.T) *etcdRegistry {
 type: etcdv3
 etcdv3:
   endpoints: 127.0.0.1:2379
+  cluster_name: test
 `
 	regCfg := config.RegistryConfig{}
 	defaults.Set(&regCfg)
@@ -45,7 +47,7 @@ etcdv3:
 	assert.NoError(t, err, "failed to recv lease response")
 	assert.NotNil(t, resp)
 
-	return &etcdRegistry{
+	r := &etcdRegistry{
 		client:      client,
 		clusterName: regCfg.ETCDConfig.ClusterName,
 		leaseWrp: leaseWrapper{
@@ -55,6 +57,11 @@ etcdv3:
 			isLeaseRunning: false,
 		},
 	}
+
+	r.leaseWrp.wg.Add(1)
+	go r.leaseKeeper()
+
+	return r
 }
 
 func TestRegister(t *testing.T) {
@@ -72,11 +79,12 @@ func TestRegister(t *testing.T) {
 	err := r.Register(addr)
 	assert.NoError(t, err)
 
-	resp, err := r.client.Get(ctx, utils.BuildRegistryValue(addr))
+	key := utils.BuildRegistryKey(r.clusterName, addr)
+	resp, err := r.client.Get(ctx, key, clientv3.WithLease(*r.leaseWrp.leaseId))
 	assert.NoError(t, err)
-	assert.NotZero(t, len(resp.Kvs))
+	assert.NotZero(t, resp.Count)
 	for _, kv := range resp.Kvs {
-		assert.Equal(t, utils.BuildRegistryValue(addr), kv.Value)
+		assert.Equal(t, utils.BuildRegistryValue(addr), string(kv.Value))
 	}
 }
 
@@ -96,5 +104,64 @@ func TestUnRegister(t *testing.T) {
 	assert.NoError(t, err)
 
 	err = r.UnRegister(addr)
+	assert.NoError(t, err)
+
+	resp, err := r.client.Get(ctx, utils.BuildRegistryKey(r.clusterName, addr), clientv3.WithLease(*r.leaseWrp.leaseId))
+	assert.NoError(t, err)
+	assert.Zero(t, resp.Count)
+
+	_, err = r.client.Delete(ctx, constant.ETCDV3_REGISTRY_PREFIX+r.clusterName, clientv3.WithLease(*r.leaseWrp.leaseId))
+	assert.NoError(t, err)
+}
+
+type mockListener struct {
+	counter     int
+	t           *testing.T
+	servicesMap map[string]string
+}
+
+func (m *mockListener) OnEvent(services []*registry.Service) error {
+	assert.NotEmpty(m.t, services)
+	for _, service := range services {
+		_, ok := m.servicesMap[service.Name]
+		assert.Equal(m.t, true, ok)
+		m.counter++
+	}
+	return nil
+}
+
+func TestSubscribe(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	r := initRegistry(ctx, t)
+	defer r.Stop()
+
+	services := []string{
+		"127.0.0.1:11451",
+		"127.0.0.1:11452",
+		"127.0.0.1:11453",
+		"127.0.0.1:11454",
+	}
+
+	servicesMap := make(map[string]string)
+
+	prefixKey := constant.ETCDV3_REGISTRY_PREFIX + r.clusterName + "-"
+	l := &mockListener{servicesMap: servicesMap, t: t, counter: 0}
+	err := r.Subscribe("", l)
+	assert.NoError(t, err)
+
+	for _, service := range services {
+		resp, err := r.client.Put(ctx, prefixKey+service, service)
+		assert.NoError(t, err)
+		assert.NotNil(t, resp)
+
+		l.servicesMap[prefixKey+service] = service
+	}
+
+	time.Sleep(1 * time.Second)
+	assert.Equal(t, len(services), l.counter)
+
+	_, err = r.client.Delete(ctx, prefixKey, clientv3.WithPrefix())
 	assert.NoError(t, err)
 }
