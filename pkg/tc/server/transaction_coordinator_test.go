8c7a74618d4befd5b97b592cf829dc148981a3b2
bohehe <hehebu33@gmail.com>
2022-02-23 22:01:49 +0800

    add TestTransactionCoordinator_BranchReport

--- pkg/tc/server/transaction_coordinator_test.go
+++ pkg/tc/server/transaction_coordinator_test.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/golang/mock/gomock"
@@ -84,6 +85,61 @@ func TestTransactionCoordinator_GetStatus(t *testing.T) {
 			tc := tt.transactionCoordinator(ctrl)
 
 			actualResp, actualErr := tc.GetStatus(tt.ctx, tt.request)
+			assert.Equal(t, tt.expectedResult, actualResp)
+			assert.Equal(t, tt.expectedErr, actualErr)
+		})
+	}
+}
+
+func TestTransactionCoordinator_BranchReport(t *testing.T) {
+	//xid := "localhost:123"
+	tests := []struct {
+		name                   string
+		transactionCoordinator func(ctrl *gomock.Controller) *TransactionCoordinator
+		ctx                    context.Context
+		request                *apis.BranchReportRequest
+		expectedResult         *apis.BranchReportResponse
+		expectedErr            error
+	}{
+		{
+			name: "test BranchReport with empty XID",
+			transactionCoordinator: func(ctrl *gomock.Controller) *TransactionCoordinator {
+				transactionCoordinator := &TransactionCoordinator{}
+				mockedSessionHolder := hmock.NewMockSessionHolderInterface(ctrl)
+
+				mockedSessionHolder.
+					EXPECT().
+					FindGlobalTransaction("").
+					Return(nil)
+
+				transactionCoordinator.holder = mockedSessionHolder
+
+				return transactionCoordinator
+			},
+			ctx: nil,
+			request: &apis.BranchReportRequest{
+				XID: "",
+			},
+			expectedResult: &apis.BranchReportResponse{
+				ResultCode:    apis.ResultCodeFailed,
+				ExceptionCode: apis.GlobalTransactionNotExist,
+				Message:       fmt.Sprintf("could not find global transaction xid = %s", ""),
+			},
+			expectedErr: nil,
+		},
+		// todo: test BranchReport with empty branch
+		// todo: test BranchReport with update branch status error
+		// todo: test BranchReport success
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			tc := tt.transactionCoordinator(ctrl)
+
+			actualResp, actualErr := tc.BranchReport(tt.ctx, tt.request)
 			assert.Equal(t, tt.expectedResult, actualResp)
 			assert.Equal(t, tt.expectedErr, actualErr)
 		})
