428c8d53a7a5fd5f097af3aa312b0d6465cd9b96
liaochuntao <liaochuntao@live.com>
2022-07-18 18:56:49 +0800

    (WIP) Feat transaction at datasource (#127)
    * realize branchReport
    
    * feat: add datasource proxy
    
    * add processor unittest
    
    * remove blank and annotation
    
    * branchReport return value judgment
    
    * feat: none
    
    * feat: refactor datasource design
    
    * docs: add code comment
    
    * refactor: rename SQLHook to SQLInterceptor
    
    * refactor: none
    
    * optimize-zap-log
    
    * update changed the format of log
    
    * remove the duplicate code same with sample/local
    
    * feat: add rm
    
    * feat: none
    
    * refactor: refactor undo log
    
    * optimize format of logging
    
    * optimize format of logging
    
    * optimize format of logging
    
    * Update logging.go
    
    * refactor: refactor db resource proxy
    
    * refactor: refactot data proxy
    
    * feature add action context for tcc
    
    * add err check for unmarshal json
    
    * feat: query use Executor
    
    * refactor: refactor code and fix bug
    
    * refactor: refactor datasource AT mode
    
    Co-authored-by: lichen <liiiiiibpm@163.com>
    Co-authored-by: springliao <springliao@tencent.com>
    Co-authored-by: cgDeepLearn <cglearningnow@163.com>
    Co-authored-by: liiibpm <105549399+liiibpm@users.noreply.github.com>
    Co-authored-by: juzimao <578961953@qq.com>
    Co-authored-by: Xin.Zh <dragoncharlie@foxmail.com>

---
+++ pkg/datasource/sql/exec/hook.go
@@ -0,0 +1,68 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package exec
+
+import (
+	"context"
+	"database/sql/driver"
+
+	"github.com/seata/seata-go-datasource/sql/types"
+)
+
+var (
+	commonHook = make([]SQLInterceptor, 0, 4)
+	hookSolts  = map[types.SQLType][]SQLInterceptor{}
+)
+
+// RegisCommonHook not goroutine safe
+func RegisCommonHook(hook SQLInterceptor) {
+	commonHook = append(commonHook, hook)
+}
+
+// RegisHook not goroutine safe
+func RegisHook(hook SQLInterceptor) {
+	_, ok := hookSolts[hook.Type()]
+
+	if !ok {
+		hookSolts[hook.Type()] = make([]SQLInterceptor, 0, 4)
+	}
+
+	hookSolts[hook.Type()] = append(hookSolts[hook.Type()], hook)
+}
+
+// ExecContext
+type ExecContext struct {
+	TxCtx       *types.TransactionContext
+	Query       string
+	NamedValues []driver.NamedValue
+	Values      []driver.Value
+}
+
+// SQLHook SQL execution front and back interceptor
+// case 1. Used to intercept SQL to achieve the generation of front and rear mirrors
+// case 2. Burning point to report
+// case 3. SQL black and white list
+type SQLInterceptor interface {
+	Type() types.SQLType
+
+	// Before
+	Before(ctx context.Context, execCtx *ExecContext)
+
+	// After
+	After(ctx context.Context, execCtx *ExecContext)
+}
---
+++ pkg/datasource/sql/types/image.go
@@ -0,0 +1,101 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package types
+
+import (
+	gosql "database/sql"
+)
+
+// RoundRecordImage Front and rear mirror data
+type RoundRecordImage struct {
+	bIndex int32
+	before RecordImages
+	aIndex int32
+	after  RecordImages
+}
+
+// AppendBeofreImage
+func (r *RoundRecordImage) AppendBeofreImage(image *RecordImage) {
+	r.bIndex++
+	image.index = r.bIndex
+
+	r.before = append(r.before, image)
+}
+
+// AppendAfterImage
+func (r *RoundRecordImage) AppendAfterImage(image *RecordImage) {
+	r.aIndex++
+	image.index = r.aIndex
+
+	r.after = append(r.after, image)
+}
+
+func (r *RoundRecordImage) BeofreImages() RecordImages {
+	return r.before
+}
+
+func (r *RoundRecordImage) AfterImages() RecordImages {
+	return r.after
+}
+
+func (r *RoundRecordImage) IsEmpty() bool {
+	return false
+}
+
+type RecordImages []*RecordImage
+
+// Reserve The order of reverse mirrors, when executing undo, needs to be executed in reverse
+func (rs RecordImages) Reserve() {
+	l := 0
+	r := len(rs) - 1
+
+	for l <= r {
+		rs[l], rs[r] = rs[r], rs[l]
+
+		l++
+		r--
+	}
+}
+
+// RecordImage
+type RecordImage struct {
+	// index
+	index int32
+	// Table
+	Table string
+	// SQLType
+	SQLType string
+	// Rows
+	Rows []RowImage
+}
+
+// RowImage Mirror data information information
+type RowImage struct {
+	// Columns All columns of image data
+	Columns []ColumnImage
+}
+
+// ColumnImage The mirror data information of the column
+type ColumnImage struct {
+	// Name column name
+	Name string
+	// Type column type
+	Type gosql.ColumnType
+	// Value column value
+	Value interface{}
+}
