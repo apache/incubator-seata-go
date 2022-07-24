44607a51e2792501e5f41c95e5e6b9d2ebe620b2
juzimao <578961953@qq.com>
2022-07-24 19:48:11 +0800

    finish ut for interceptor

---
+++ pkg/integration/grpc/client/client_transaction_interceptor.go
@@ -0,0 +1,47 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package client
+
+import (
+	"context"
+	"time"
+
+	"github.com/seata/seata-go/pkg/common/log"
+	grpc2 "github.com/seata/seata-go/pkg/integration/grpc"
+	"github.com/seata/seata-go/pkg/tm"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+)
+
+func ClientTransactionInterceptor(ctx context.Context, method string, req, reply interface{},
+	cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+	//set the XID when intercepting a client request and release it directly when intercepting a response
+	if tm.IsSeataContext(ctx) {
+		xid := tm.GetXID(ctx)
+		header := make(map[string]string)
+		header[grpc2.HEADER_KEY] = xid
+		ctx = metadata.NewOutgoingContext(ctx, metadata.New(header))
+	}
+
+	start := time.Now()
+	err := invoker(ctx, method, req, reply, cc, opts...)
+	end := time.Now()
+	log.Infof("RPC: %s, start time: %s, end time: %s, err: %v", method,
+		start.Format("Basic"), end.Format(time.RFC3339), err)
+	return err
+}
---
+++ pkg/integration/grpc/grpc_test.go
@@ -0,0 +1,85 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package grpc
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"testing"
+
+	"github.com/seata/seata-go/pkg/tm"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/seata/seata-go/pkg/common/log"
+	"github.com/seata/seata-go/pkg/integration/grpc/pb"
+	"google.golang.org/grpc"
+)
+
+type ContextRpcTestServer struct {
+	pb.UnimplementedContextRpcServer
+}
+
+func (c *ContextRpcTestServer) ContextRpc(ctx context.Context, req *pb.Request) (*pb.Response, error) {
+	log.Infof("receive the %s", req.Name)
+	return &pb.Response{Greet: fmt.Sprintf("receive the name %s, xid is %s, return greet!", req.Name, tm.GetXID(ctx))}, nil
+}
+
+func TestClientHeaderDeliveredToServer(t *testing.T) {
+	StartServer(t)
+	StartClientWithCall(t)
+}
+
+// StartServer to start grpc server
+func StartServer(t *testing.T) {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 44451))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+		t.FailNow()
+	}
+	grpcServer := grpc.NewServer()
+	pb.RegisterContextRpcServer(grpcServer, &ContextRpcTestServer{})
+
+	go func() {
+		grpcServer.Serve(lis)
+	}()
+}
+
+// StartClientWithCall to start grpc client and call server
+func StartClientWithCall(t *testing.T) {
+
+	conn, err := grpc.Dial("localhost:44451")
+	if err != nil {
+		log.Fatalf("dial to server: %v", err)
+		t.FailNow()
+	}
+	defer conn.Close()
+	contextRpcClient := pb.NewContextRpcClient(conn)
+	ctx := context.Background()
+	ctx = tm.InitSeataContext(ctx)
+	tm.SetXID(ctx, "111111")
+	response, err := contextRpcClient.ContextRpc(ctx, &pb.Request{Name: "zhangsan"})
+
+	if err != nil {
+		log.Fatalf("call rpc : %v", err)
+		t.FailNow()
+	}
+
+	assert.Equal(t, "receive the name zhangsan, xid is 111111, return greet!", response.Greet)
+
+}
