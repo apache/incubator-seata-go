be6fc2cbf17f389f272a07ce7b716c369708a36e
lichen <liiiiiibpm@163.com>
2022-08-07 18:14:27 +0800

    optimiz: optimize function's parameters into one struct-rm_api.go

--- pkg/rm/rm_api.go
+++ pkg/rm/rm_api.go
@@ -31,12 +31,20 @@ type Resource interface {
 	GetBranchType() branch.BranchType
 }
 
+type BranchResource struct {
+	BranchType      branch.BranchType
+	Xid             string
+	BranchId        int64
+	ResourceId      string
+	ApplicationData []byte
+}
+
 // Control a branch transaction commit or rollback
 type ResourceManagerInbound interface {
 	// Commit a branch transaction
-	BranchCommit(ctx context.Context, branchType branch.BranchType, xid string, branchId int64, resourceId string, applicationData []byte) (branch.BranchStatus, error)
+	BranchCommit(ctx context.Context, resource BranchResource) (branch.BranchStatus, error)
 	// Rollback a branch transaction
-	BranchRollback(ctx context.Context, ranchType branch.BranchType, xid string, branchId int64, resourceId string, applicationData []byte) (branch.BranchStatus, error)
+	BranchRollback(ctx context.Context, resource BranchResource) (branch.BranchStatus, error)
 }
 
 // Resource Manager: send outbound request to TC
