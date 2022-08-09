33c2bd354aa28b1c466aa1628726874c40f549fc
xubaisheng <>
2022-08-10 00:12:10 +0800

    getty tests

---
+++ pkg/remoting/getty/getty_remoting_test.go
@@ -0,0 +1,173 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package getty
+
+import (
+	_ "github.com/golang/mock/mockgen/model"
+	"github.com/seata/seata-go/pkg/protocol/message"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestGettyRemoting_GetMessageFuture(t *testing.T) {
+	var tests = []struct {
+		name          string
+		msgID         int32
+		messageFuture *message.MessageFuture
+	}{
+		{
+			name:          "futures is null",
+			msgID:         1,
+			messageFuture: nil,
+		},
+		{
+			name:  "futures not  null",
+			msgID: 1,
+			messageFuture: &message.MessageFuture{
+				ID:       1,
+				Err:      nil,
+				Response: nil,
+				Done:     nil,
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			if test.messageFuture != nil {
+				GetGettyRemotingInstance().futures.Store(test.msgID, test.messageFuture)
+				messageFuture := GetGettyRemotingInstance().GetMessageFuture(test.msgID)
+				assert.Equal(t, *test.messageFuture, *messageFuture)
+			} else {
+				messageFuture := GetGettyRemotingInstance().GetMessageFuture(test.msgID)
+				assert.Empty(t, messageFuture)
+			}
+		})
+	}
+}
+
+func TestGettyRemoting_RemoveMessageFuture(t *testing.T) {
+	var tests = []struct {
+		name          string
+		msgID         int32
+		messageFuture *message.MessageFuture
+	}{
+		{
+			name:          "futures is null",
+			msgID:         1,
+			messageFuture: nil,
+		},
+		{
+			name:  "futures not  null",
+			msgID: 1,
+			messageFuture: &message.MessageFuture{
+				ID:       1,
+				Err:      nil,
+				Response: nil,
+				Done:     nil,
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			if test.messageFuture != nil {
+				GetGettyRemotingInstance().futures.Store(test.msgID, test.messageFuture)
+				messageFuture := GetGettyRemotingInstance().GetMessageFuture(test.msgID)
+				assert.NotEmpty(t, messageFuture)
+				GetGettyRemotingInstance().RemoveMessageFuture(test.msgID)
+				messageFuture = GetGettyRemotingInstance().GetMessageFuture(test.msgID)
+				assert.Empty(t, messageFuture)
+			} else {
+				GetGettyRemotingInstance().RemoveMessageFuture(test.msgID)
+				messageFuture := GetGettyRemotingInstance().GetMessageFuture(test.msgID)
+				assert.Empty(t, messageFuture)
+			}
+		})
+	}
+}
+
+func TestGettyRemoting_GetMergedMessage(t *testing.T) {
+	var tests = []struct {
+		name              string
+		msgID             int32
+		mergedWarpMessage *message.MergedWarpMessage
+	}{
+		{
+			name:              "mergeMsgMap is null",
+			msgID:             1,
+			mergedWarpMessage: nil,
+		},
+		{
+			name:  "mergeMsgMap not  null",
+			msgID: 1,
+			mergedWarpMessage: &message.MergedWarpMessage{
+				Msgs:   []message.MessageTypeAware{},
+				MsgIds: []int32{1, 2},
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			if test.mergedWarpMessage != nil {
+				GetGettyRemotingInstance().mergeMsgMap.Store(test.msgID, test.mergedWarpMessage)
+				mergedWarpMessage := GetGettyRemotingInstance().GetMergedMessage(test.msgID)
+				assert.Equal(t, *test.mergedWarpMessage, *mergedWarpMessage)
+			} else {
+				mergedWarpMessage := GetGettyRemotingInstance().GetMessageFuture(test.msgID)
+				assert.Empty(t, mergedWarpMessage)
+			}
+		})
+	}
+}
+
+func TestGettyRemoting_RemoveMergedMessageFuture(t *testing.T) {
+	var tests = []struct {
+		name              string
+		msgID             int32
+		mergedWarpMessage *message.MergedWarpMessage
+	}{
+		{
+			name:              "mergeMsgMap is null",
+			msgID:             1,
+			mergedWarpMessage: nil,
+		},
+		{
+			name:  "mergeMsgMap not  null",
+			msgID: 1,
+			mergedWarpMessage: &message.MergedWarpMessage{
+				Msgs:   []message.MessageTypeAware{},
+				MsgIds: []int32{1, 2},
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			if test.mergedWarpMessage != nil {
+				GetGettyRemotingInstance().mergeMsgMap.Store(test.msgID, test.mergedWarpMessage)
+				mergedWarpMessage := GetGettyRemotingInstance().GetMergedMessage(test.msgID)
+				assert.NotEmpty(t, mergedWarpMessage)
+				GetGettyRemotingInstance().RemoveMergedMessageFuture(test.msgID)
+				mergedWarpMessage = GetGettyRemotingInstance().GetMergedMessage(test.msgID)
+				assert.Empty(t, mergedWarpMessage)
+			} else {
+				GetGettyRemotingInstance().RemoveMergedMessageFuture(test.msgID)
+				mergedWarpMessage := GetGettyRemotingInstance().GetMergedMessage(test.msgID)
+				assert.Empty(t, mergedWarpMessage)
+			}
+		})
+	}
+}
