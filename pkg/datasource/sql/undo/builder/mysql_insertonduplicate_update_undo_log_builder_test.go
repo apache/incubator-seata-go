ed5b7858644aed1a800a2f3fa46ea5741d5a9e84
kirhaku <2454546080@qq.com>
2022-10-15 19:55:03 +0800

    add insertonduplicate_update test

---
+++ pkg/datasource/sql/undo/builder/mysql_insertonduplicate_update_undo_log_builder_test.go
@@ -0,0 +1,102 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package builder
+
+import (
+	"database/sql/driver"
+	"testing"
+
+	"github.com/seata/seata-go/pkg/datasource/sql/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInsertOnDuplicateBuildBeforeImageSQL(t *testing.T) {
+	var (
+		builder     = MySQLInsertOnDuplicateUndoLogBuilder{}
+		tableMeta   types.TableMeta
+		columns     = make(map[string]types.ColumnMeta)
+		index       = make(map[string]types.IndexMeta)
+		columnMeta1 []types.ColumnMeta
+		columnMeta2 []types.ColumnMeta
+	)
+	columnId := types.ColumnMeta{
+		ColumnDef:  nil,
+		ColumnName: "id",
+	}
+	columnName := types.ColumnMeta{
+		ColumnDef:  nil,
+		ColumnName: "name",
+	}
+	columnAge := types.ColumnMeta{
+		ColumnDef:  nil,
+		ColumnName: "age",
+	}
+	columns["id"] = columnId
+	columns["name"] = columnName
+	columns["age"] = columnAge
+	columnMeta1 = append(columnMeta1, columnId)
+	columnMeta2 = append(columnMeta2, columnName, columnAge)
+	index["id"] = types.IndexMeta{
+		Name:   "PRIMARY",
+		IType:  types.IndexTypePrimaryKey,
+		Values: columnMeta1,
+	}
+	index["id_name_age"] = types.IndexMeta{
+		Name:   "name_age_idx",
+		IType:  types.IndexTypeUniqueKey,
+		Values: columnMeta2,
+	}
+
+	tableMeta = types.TableMeta{
+		Name:    "t_user",
+		Columns: columns,
+		Indexs:  index,
+	}
+
+	tests := []struct {
+		execCtx          *types.ExecContext
+		sourceQueryArgs  []driver.Value
+		expectQuery1     string
+		expectQueryArgs1 []driver.Value
+		expectQuery2     string
+		expectQueryArgs2 []driver.Value
+	}{
+		{
+			execCtx: &types.ExecContext{
+				Query:    "insert into t_user(id, name, age) values(?,?,?) on duplicate key update name = ?,age = ?",
+				MetaData: tableMeta,
+			},
+			sourceQueryArgs:  []driver.Value{1, "Jack1", 81, "Link", 18},
+			expectQuery1:     "SELECT * FROM t_user  WHERE (id = ? )  OR (name = ?  and age = ? ) ",
+			expectQueryArgs1: []driver.Value{1, "Jack1", 81},
+			expectQuery2:     "SELECT * FROM t_user  WHERE (name = ?  and age = ? )  OR (id = ? ) ",
+			expectQueryArgs2: []driver.Value{"Jack1", 81, 1},
+		},
+	}
+	for _, tt := range tests {
+		query, args, err := builder.buildBeforeImageSQL(tt.execCtx, tt.sourceQueryArgs)
+		assert.Nil(t, err)
+		if query == tt.expectQuery1 {
+			assert.Equal(t, tt.expectQuery1, query)
+			assert.Equal(t, tt.expectQueryArgs1, args)
+		} else {
+			assert.Equal(t, tt.expectQuery2, query)
+			assert.Equal(t, tt.expectQueryArgs2, args)
+		}
+	}
+}
