bba1816165893f9a8e6452a7bc5a0a385352cdbc
georgehao <haohongfan@gmail.com>
2022-11-20 22:57:21 +0800

    feat: update

--- sample/at/rollback/server2/service.go
+++ sample/at/rollback/server2/service.go
@@ -43,13 +43,13 @@ func updateDataFail(ctx context.Context) error {
 	ret, err := db.ExecContext(ctx, sql, fmt.Sprintf("NewDescs1-%d", time.Now().UnixMilli()), 10000)
 	if err != nil {
 		fmt.Printf("update failed, err:%v\n", err)
-		return nil
+		return err
 	}
 
 	rows, err := ret.RowsAffected()
 	if err != nil {
 		fmt.Printf("update failed, err:%v\n", err)
-		return nil
+		return err
 	}
 	fmt.Printf("update success： %d.\n", rows)
 	return nil
