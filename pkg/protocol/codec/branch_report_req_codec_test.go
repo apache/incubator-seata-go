268a72e65e176040ea6b286915e08e19b935725b
Yuecai Liu <38887641+luky116@users.noreply.github.com>
2022-11-20 10:50:33 +0800

    optimize commit (#354)
    optimize commit

---
+++ pkg/protocol/codec/branch_report_req_codec_test.go
@@ -0,0 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package codec
+
+import (
+	"testing"
+
+	model2 "github.com/seata/seata-go/pkg/protocol/branch"
+	"github.com/seata/seata-go/pkg/protocol/message"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBranchReportRequestCodec(t *testing.T) {
+	msg := message.BranchReportRequest{
+		Xid:             "123344",
+		ResourceId:      "root:12345678@tcp(127.0.0.1:3306)/seata_client",
+		Status:          model2.BranchStatusPhaseoneDone,
+		BranchId:        56678,
+		BranchType:      model2.BranchTypeAT,
+		ApplicationData: []byte("TestExtraData"),
+	}
+
+	codec := BranchReportRequestCodec{}
+	bytes := codec.Encode(msg)
+	msg2 := codec.Decode(bytes)
+
+	assert.Equal(t, msg, msg2)
+}
