90634ed109273357c1dcfdce233ccf962dcaeccd
liiibpm <liiibpm@163.com>
2022-12-25 19:26:16 +0800

    add LockConfig's RegisterFlagsWithPrefix

--- pkg/rm/config.go
+++ pkg/rm/config.go
@@ -23,16 +23,16 @@ import (
 )
 
 type Config struct {
-	AsyncCommitBufferLimit          int        `yaml:"async_commit_buffer_limit" json:"async_commit_buffer_limit,omitempty" koanf:"async_commit_buffer_limit"`
-	ReportRetryCount                int        `yaml:"report_retry_count" json:"report_retry_count,omitempty" koanf:"report_retry_count"`
-	TableMetaCheckEnable            bool       `yaml:"table_meta_check_enable" json:"table_meta_check_enable" koanf:"table_meta_check_enable"`
-	ReportSuccessEnable             bool       `yaml:"report_success_enable" json:"report_success_enable,omitempty" koanf:"report_success_enable"`
-	SagaBranchRegisterEnable        bool       `yaml:"saga_branch_register_enable" json:"saga_branch_register_enable,omitempty" koanf:"saga_branch_register_enable"`
-	SagaJsonParser                  string     `yaml:"saga_json_parser" json:"saga_json_parser,omitempty" koanf:"saga_json_parser"`
-	SagaRetryPersistModeUpdate      bool       `yaml:"saga_retry_persist_mode_update" json:"saga_retry_persist_mode_update,omitempty" koanf:"saga_retry_persist_mode_update"`
-	SagaCompensatePersistModeUpdate bool       `yaml:"saga_compensate_persist_mode_update" json:"saga_compensate_persist_mode_update,omitempty" koanf:"saga_compensate_persist_mode_update"`
-	TccActionInterceptorOrder       int        `yaml:"tcc_action_interceptor_order" json:"tcc_action_interceptor_order,omitempty" koanf:"tcc_action_interceptor_order"`
-	SqlParserType                   string     `yaml:"sql_parser_type" json:"sql_parser_type,omitempty" koanf:"sql_parser_type"`
+	AsyncCommitBufferLimit          int        `yaml:"async-commit-buffer-limit" json:"async-commit-buffer-limit,omitempty" koanf:"async-commit-buffer-limit"`
+	ReportRetryCount                int        `yaml:"report-retry-count" json:"report-retry-count,omitempty" koanf:"report-retry-count"`
+	TableMetaCheckEnable            bool       `yaml:"table-meta-check-enable" json:"table-meta-check-enable" koanf:"table-meta-check-enable"`
+	ReportSuccessEnable             bool       `yaml:"report-success-enable" json:"report-success-enable,omitempty" koanf:"report-success-enable"`
+	SagaBranchRegisterEnable        bool       `yaml:"saga-branch-register-enable" json:"saga-branch-register-enable,omitempty" koanf:"saga-branch-register-enable"`
+	SagaJsonParser                  string     `yaml:"saga-json-parser" json:"saga-json-parser,omitempty" koanf:"saga-json-parser"`
+	SagaRetryPersistModeUpdate      bool       `yaml:"saga-retry-persist-mode-update" json:"saga-retry-persist-mode-update,omitempty" koanf:"saga-retry-persist-mode-update"`
+	SagaCompensatePersistModeUpdate bool       `yaml:"saga-compensate-persist-mode-update" json:"saga-compensate-persist-mode-update,omitempty" koanf:"saga-compensate-persist-mode-update"`
+	TccActionInterceptorOrder       int        `yaml:"tcc-action-interceptor-order" json:"tcc-action-interceptor-order,omitempty" koanf:"tcc-action-interceptor-order"`
+	SqlParserType                   string     `yaml:"sql-parser-type" json:"sql-parser-type,omitempty" koanf:"sql-parser-type"`
 	LockConfig                      LockConfig `yaml:"lock" json:"lock,omitempty" koanf:"lock"`
 }
 
@@ -43,17 +43,21 @@ type LockConfig struct {
 }
 
 func (cfg *Config) RegisterFlagsWithPrefix(prefix string, f *flag.FlagSet) {
-	f.IntVar(&cfg.AsyncCommitBufferLimit, prefix+".async_commit_buffer_limit", 10000, "The Maximum cache length of asynchronous queue.")
-	f.IntVar(&cfg.ReportRetryCount, prefix+".report_retry_count", 5, "The maximum number of retries when report reports the status.")
-	f.BoolVar(&cfg.TableMetaCheckEnable, prefix+".table_meta_check_enable", false, "Whether to check the metadata of the db（AT）.")
-	f.BoolVar(&cfg.ReportSuccessEnable, prefix+".report_success_enable", false, "Whether to report the status if the transaction is successfully executed（AT）")
-	f.BoolVar(&cfg.SagaBranchRegisterEnable, prefix+".saga_branch_register_enable", false, "Whether to allow regular check of db metadata（AT）")
-	f.StringVar(&cfg.SagaJsonParser, prefix+".saga_json_parser", "fastjson", "The saga JsonParser.")
-	f.BoolVar(&cfg.SagaRetryPersistModeUpdate, prefix+".saga_retry_persist_mode_update", false, "Whether to retry SagaRetryPersistModeUpdate")
-	f.BoolVar(&cfg.SagaCompensatePersistModeUpdate, prefix+".saga_compensate_persist_mode_update", false, "")
-	f.IntVar(&cfg.TccActionInterceptorOrder, prefix+".tcc_action_interceptor_order", -2147482648, "The order of tccActionInterceptor.")
-	f.StringVar(&cfg.SqlParserType, prefix+".sql_parser_type", "druid", "The type of sql parser.")
-	f.IntVar(&cfg.LockConfig.RetryInterval, prefix+".retry-interval", 10, "The maximum number of retries when lock fail.")
-	f.DurationVar(&cfg.LockConfig.RetryTimes, prefix+".retry-times", 30*time.Second, "The duration allowed for lock retrying.")
-	f.BoolVar(&cfg.LockConfig.RetryPolicyBranchRollbackOnConflict, prefix+".retry-policy-branch-rollback-on-conflict", true, "The switch for lock conflict.")
+	f.IntVar(&cfg.AsyncCommitBufferLimit, prefix+".async-commit-buffer-limit", 10000, "The Maximum cache length of asynchronous queue.")
+	f.IntVar(&cfg.ReportRetryCount, prefix+".report-retry-count", 5, "The maximum number of retries when report reports the status.")
+	f.BoolVar(&cfg.TableMetaCheckEnable, prefix+".table-meta-check-enable", false, "Whether to check the metadata of the db（AT）.")
+	f.BoolVar(&cfg.ReportSuccessEnable, prefix+".report-success-enable", false, "Whether to report the status if the transaction is successfully executed（AT）")
+	f.BoolVar(&cfg.SagaBranchRegisterEnable, prefix+".saga-branch-register-enable", false, "Whether to allow regular check of db metadata（AT）")
+	f.StringVar(&cfg.SagaJsonParser, prefix+".saga-json-parser", "fastjson", "The saga JsonParser.")
+	f.BoolVar(&cfg.SagaRetryPersistModeUpdate, prefix+".saga-retry-persist-mode-update", false, "Whether to retry SagaRetryPersistModeUpdate")
+	f.BoolVar(&cfg.SagaCompensatePersistModeUpdate, prefix+".saga-compensate-persist-mode-update", false, "")
+	f.IntVar(&cfg.TccActionInterceptorOrder, prefix+".tcc-action-interceptor-order", -2147482648, "The order of tccActionInterceptor.")
+	f.StringVar(&cfg.SqlParserType, prefix+".sql-parser-type", "druid", "The type of sql parser.")
+	cfg.LockConfig.RegisterFlagsWithPrefix(prefix, f)
+}
+
+func (cfg *LockConfig) RegisterFlagsWithPrefix(prefix string, f *flag.FlagSet) {
+	f.IntVar(&cfg.RetryInterval, prefix+".retry-interval", 10, "The maximum number of retries when lock fail.")
+	f.DurationVar(&cfg.RetryTimes, prefix+".retry-times", 30*time.Second, "The duration allowed for lock retrying.")
+	f.BoolVar(&cfg.RetryPolicyBranchRollbackOnConflict, prefix+".retry-policy-branch-rollback-on-conflict", true, "The switch for lock conflict.")
 }
