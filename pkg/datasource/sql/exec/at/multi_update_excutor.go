7fa450e34d8efe835ec8a75df6cd9aeeb7c55aae
唯德 <qinkai.qin@alibaba-inc.com>
2023-02-15 11:53:32 +0800

    fix:multiple update executor code review

--- pkg/datasource/sql/exec/at/multi_update_excutor.go
+++ pkg/datasource/sql/exec/at/multi_update_excutor.go
@@ -1,48 +1,73 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package at
 
 import (
 	"context"
 	"database/sql/driver"
 	"fmt"
+	"strings"
+
 	"github.com/arana-db/parser"
 	"github.com/arana-db/parser/ast"
 	"github.com/arana-db/parser/format"
 	"github.com/arana-db/parser/model"
 	"github.com/pkg/errors"
+	"github.com/seata/seata-go/pkg/constant"
+	"github.com/seata/seata-go/pkg/datasource/sql/datasource"
 	"github.com/seata/seata-go/pkg/datasource/sql/exec"
 	"github.com/seata/seata-go/pkg/datasource/sql/types"
 	"github.com/seata/seata-go/pkg/datasource/sql/undo"
 	"github.com/seata/seata-go/pkg/datasource/sql/util"
 	"github.com/seata/seata-go/pkg/util/bytes"
 	"github.com/seata/seata-go/pkg/util/log"
-	"strings"
 )
 
-// MultiUpdateExecutor execute multiple update SQL
-type MultiUpdateExecutor struct {
+var (
+	rows    driver.Rows
+	queryer driver.Queryer
+)
+
+// multiUpdateExecutor execute multiple update SQL
+type multiUpdateExecutor struct {
 	baseExecutor
 	parserCtx   *types.ParseContext
 	execContext *types.ExecContext
 }
 
 // NewMultiUpdateExecutor get new multi update executor
-func NewMultiUpdateExecutor(parserCtx *types.ParseContext, execContext *types.ExecContext, hooks []exec.SQLHook) *MultiUpdateExecutor {
-	return &MultiUpdateExecutor{parserCtx: parserCtx, execContext: execContext, baseExecutor: baseExecutor{hooks: hooks}}
+func NewMultiUpdateExecutor(parserCtx *types.ParseContext, execContext *types.ExecContext, hooks []exec.SQLHook) multiUpdateExecutor {
+	return multiUpdateExecutor{parserCtx: parserCtx, execContext: execContext, baseExecutor: baseExecutor{hooks: hooks}}
 }
 
 // ExecContext exec SQL, and generate before image and after image
-func (u *MultiUpdateExecutor) ExecContext(ctx context.Context, f exec.CallbackWithNamedValue) (types.ExecResult, error) {
+func (u *multiUpdateExecutor) ExecContext(ctx context.Context, f exec.CallbackWithNamedValue) (types.ExecResult, error) {
 	u.beforeHooks(ctx, u.execContext)
+
+	defer func() {
+		u.afterHooks(ctx, u.execContext)
+	}()
 
 	//single update sql handler
 	if len(u.execContext.ParseContext.MultiStmt) == 1 {
 		u.execContext.ParseContext.UpdateStmt = u.execContext.ParseContext.MultiStmt[0].UpdateStmt
 		return NewUpdateExecutor(u.parserCtx, u.execContext, u.hooks).ExecContext(ctx, f)
 	}
-
-	defer func() {
-		u.afterHooks(ctx, u.execContext)
-	}()
 
 	beforeImages, err := u.beforeImage(ctx)
 	if err != nil {
@@ -54,7 +79,7 @@ func (u *MultiUpdateExecutor) ExecContext(ctx context.Context, f exec.CallbackWi
 		return nil, err
 	}
 
-	afterImages, err := u.afterImage(beforeImages)
+	afterImages, err := u.afterImage(ctx, beforeImages)
 	if err != nil {
 		return nil, err
 	}
@@ -76,60 +101,41 @@ func (u *MultiUpdateExecutor) ExecContext(ctx context.Context, f exec.CallbackWi
 	return res, nil
 }
 
-func (u *MultiUpdateExecutor) beforeImage(ctx context.Context) ([]*types.RecordImage, error) {
+func (u *multiUpdateExecutor) beforeImage(ctx context.Context) ([]*types.RecordImage, error) {
 	if !u.isAstStmtValid() {
 		return nil, nil
 	}
 
-	var updateStmts []*ast.UpdateStmt
-	for _, v := range u.parserCtx.MultiStmt {
-		updateStmts = append(updateStmts, v.UpdateStmt)
-	}
-
 	tableName := u.execContext.ParseContext.UpdateStmt.TableRefs.TableRefs.Left.(*ast.TableSource).Source.(*ast.TableName).Name.O
-	metaData := u.execContext.MetaDataMap[tableName]
+	metaData, err := datasource.GetTableCache(types.DBTypeMySQL).GetTableMeta(ctx, u.execContext.DBName, tableName)
+	if err != nil {
+		return nil, err
+	}
 
 	// use
-	selectSQL, selectArgs, err := u.buildBeforeImageSQL(updateStmts, u.execContext.Values, metaData)
+	selectSQL, selectArgs, err := u.buildBeforeImageSQL(u.execContext.NamedValues, metaData)
 	if err != nil {
 		return nil, err
 	}
 
-	var rows driver.Rows
-	queryerContext, ok := u.execContext.Conn.(driver.QueryerContext)
-	var queryer driver.Queryer
-	if !ok {
-		queryer, ok = u.execContext.Conn.(driver.Queryer)
-	}
-	if ok {
-		rows, err = util.CtxDriverQuery(ctx, queryerContext, queryer, selectSQL, util.ValueToNamedValue(selectArgs))
-		defer func() {
-			if rows != nil {
-				rows.Close()
-			}
-		}()
-		if err != nil {
-			log.Errorf("ctx driver query: %+v", err)
-			return nil, err
-		}
-	} else {
-		log.Errorf("target conn should been driver.QueryerContext or driver.Queryer")
-		return nil, fmt.Errorf("invalid conn")
-	}
-
-	image, err := u.buildRecordImages(rows, &metaData)
+	rows, err := u.rowsPrepare(ctx, selectSQL, selectArgs)
 	if err != nil {
 		return nil, err
 	}
 
-	lockKey := u.buildLockKey(image, metaData)
+	image, err := u.buildRecordImages(rows, metaData)
+	if err != nil {
+		return nil, err
+	}
+
+	lockKey := u.buildLockKey(image, *metaData)
 	u.execContext.TxCtx.LockKeys[lockKey] = struct{}{}
 	image.SQLType = u.parserCtx.SQLType
 
 	return []*types.RecordImage{image}, nil
 }
 
-func (u *MultiUpdateExecutor) afterImage(beforeImages []*types.RecordImage) ([]*types.RecordImage, error) {
+func (u *multiUpdateExecutor) afterImage(ctx context.Context, beforeImages []*types.RecordImage) ([]*types.RecordImage, error) {
 	if !u.isAstStmtValid() {
 		return nil, nil
 	}
@@ -140,22 +146,20 @@ func (u *MultiUpdateExecutor) afterImage(beforeImages []*types.RecordImage) ([]*
 	}
 
 	tableName := u.execContext.ParseContext.UpdateStmt.TableRefs.TableRefs.Left.(*ast.TableSource).Source.(*ast.TableName).Name.O
-	metaData := u.execContext.MetaDataMap[tableName]
-	selectSQL, selectArgs := u.buildAfterImageSQL(beforeImage, metaData)
-
-	stmt, err := u.execContext.Conn.Prepare(selectSQL)
+	metaData, err := datasource.GetTableCache(types.DBTypeMySQL).GetTableMeta(ctx, u.execContext.DBName, tableName)
 	if err != nil {
-		log.Errorf("build prepare stmt: %+v", err)
 		return nil, err
 	}
 
-	rows, err := stmt.Query(selectArgs)
+	// use
+	selectSQL, selectArgs := u.buildAfterImageSQL(beforeImage, *metaData)
+
+	rows, err := u.rowsPrepare(ctx, selectSQL, selectArgs)
 	if err != nil {
-		log.Errorf("stmt query: %+v", err)
 		return nil, err
 	}
 
-	image, err := u.buildRecordImages(rows, &metaData)
+	image, err := u.buildRecordImages(rows, metaData)
 	if err != nil {
 		return nil, err
 	}
@@ -164,45 +168,68 @@ func (u *MultiUpdateExecutor) afterImage(beforeImages []*types.RecordImage) ([]*
 	return []*types.RecordImage{image}, nil
 }
 
+func (u *multiUpdateExecutor) rowsPrepare(ctx context.Context, selectSQL string, selectArgs []driver.NamedValue) (driver.Rows, error) {
+	queryerContext, ok := u.execContext.Conn.(driver.QueryerContext)
+
+	if !ok {
+		queryer, ok = u.execContext.Conn.(driver.Queryer)
+	}
+	if ok {
+		rows, err := util.CtxDriverQuery(ctx, queryerContext, queryer, selectSQL, selectArgs)
+		defer func() {
+			if err = rows.Close(); err != nil {
+				log.Errorf("rows close fail, err:%v", err)
+				return
+			}
+		}()
+		if err != nil {
+			log.Errorf("ctx driver query: %+v", err)
+			return nil, err
+		}
+	} else {
+		log.Errorf("target conn should been driver.QueryerContext or driver.Queryer")
+		return nil, fmt.Errorf("invalid conn")
+	}
+	return rows, nil
+}
+
 // buildAfterImageSQL build the SQL to query after image data
-func (u *MultiUpdateExecutor) buildAfterImageSQL(beforeImage *types.RecordImage, meta types.TableMeta) (string, []driver.Value) {
+func (u *multiUpdateExecutor) buildAfterImageSQL(beforeImage *types.RecordImage, meta types.TableMeta) (string, []driver.NamedValue) {
 	if !u.isAstStmtValid() {
 		return "", nil
 	}
 
-	sb := strings.Builder{}
-	var selectFields string
-	var separator = ","
+	selectSql, selectFields := strings.Builder{}, strings.Builder{}
+	var selectFieldsStr string
 
 	if undo.UndoConfig.OnlyCareUpdateColumns {
 		for _, row := range beforeImage.Rows {
 			for _, column := range row.Columns {
-				selectFields += column.ColumnName + separator
+				selectFields.WriteString(column.ColumnName + constant.Comma)
 			}
 		}
-		selectFields = strings.TrimSuffix(selectFields, separator)
+		selectFieldsStr = selectFields.String()
 	} else {
-		selectFields = strings.Join(meta.ColumnNames, separator)
+		selectFieldsStr = strings.Join(meta.ColumnNames, constant.Comma)
 	}
-
-	sb.WriteString("SELECT " + selectFields + " FROM " + meta.TableName + " WHERE ")
+	selectSql.WriteString("SELECT " + selectFieldsStr + " FROM " + meta.TableName + " WHERE ")
 	whereSQL := u.buildWhereConditionByPKs(meta.GetPrimaryKeyOnlyName(), len(beforeImage.Rows), "mysql", maxInSize)
-	sb.WriteString(" " + whereSQL + " ")
-	return sb.String(), util.NamedValueToValue(u.buildPKParams(beforeImage.Rows, meta.GetPrimaryKeyOnlyName()))
+	selectSql.WriteString(" " + whereSQL + " ")
+	return selectSql.String(), u.buildPKParams(beforeImage.Rows, meta.GetPrimaryKeyOnlyName())
 }
 
 // buildSelectSQLByUpdate build select sql from update sql
-func (u *MultiUpdateExecutor) buildBeforeImageSQL(_ []*ast.UpdateStmt, args []driver.Value, meta types.TableMeta) (string, []driver.Value, error) {
+func (u *multiUpdateExecutor) buildBeforeImageSQL(args []driver.NamedValue, meta *types.TableMeta) (string, []driver.NamedValue, error) {
 	if !u.isAstStmtValid() {
 		log.Errorf("invalid multi update stmt")
 		return "", nil, fmt.Errorf("invalid muliti update stmt")
 	}
 
-	var newArgs []driver.Value
-	var fields []*ast.SelectField
-	fieldsExits := make(map[string]struct{})
 	var whereCondition strings.Builder
 	multiStmts := u.parserCtx.MultiStmt
+	newArgs := make([]driver.NamedValue, 0, len(multiStmts))
+	fields := make([]*ast.SelectField, 0, len(meta.ColumnNames))
+	fieldsExits := make(map[string]struct{}, len(meta.ColumnNames))
 
 	for _, multiStmt := range multiStmts {
 		updateStmt := multiStmt.UpdateStmt
@@ -219,25 +246,14 @@ func (u *MultiUpdateExecutor) buildBeforeImageSQL(_ []*ast.UpdateStmt, args []dr
 			}
 
 			fieldsExits[column.Column.String()] = struct{}{}
-			fields = append(fields, &ast.SelectField{
-				Expr: &ast.ColumnNameExpr{
-					Name: column.Column,
-				},
-			})
+			fields = append(fields, &ast.SelectField{Expr: &ast.ColumnNameExpr{Name: column.Column}})
 		}
 
 		if !undo.UndoConfig.OnlyCareUpdateColumns {
-			fields = make([]*ast.SelectField, len(meta.Columns))
+			fields = make([]*ast.SelectField, 0, len(meta.ColumnNames))
 			for _, column := range meta.ColumnNames {
 				fields = append(fields, &ast.SelectField{
-					Expr: &ast.ColumnNameExpr{
-						Name: &ast.ColumnName{
-							Name: model.CIStr{
-								O: column,
-							},
-						},
-					},
-				})
+					Expr: &ast.ColumnNameExpr{Name: &ast.ColumnName{Name: model.CIStr{O: column}}}})
 			}
 		}
 
@@ -253,7 +269,7 @@ func (u *MultiUpdateExecutor) buildBeforeImageSQL(_ []*ast.UpdateStmt, args []dr
 				LockType: ast.SelectLockForUpdate,
 			},
 		}
-		newArgs = append(newArgs, util.NamedValueToValue(u.buildSelectArgs(&tmpSelectStmt, util.ValueToNamedValue(args)))...)
+		newArgs = append(newArgs, u.buildSelectArgs(&tmpSelectStmt, args)...)
 
 		in := bytes.NewByteBuffer([]byte{})
 		_ = updateStmt.Where.Restore(format.NewRestoreCtx(format.RestoreKeyWordUppercase, in))
@@ -299,7 +315,7 @@ func (u *MultiUpdateExecutor) buildBeforeImageSQL(_ []*ast.UpdateStmt, args []dr
 	return sql, newArgs, nil
 }
 
-func (u *MultiUpdateExecutor) isAstStmtValid() bool {
+func (u *multiUpdateExecutor) isAstStmtValid() bool {
 	return u.parserCtx != nil && u.parserCtx.MultiStmt != nil && len(u.parserCtx.MultiStmt) > 0
 }
 
@@ -307,11 +323,11 @@ type updateVisitor struct {
 	stmt *ast.UpdateStmt
 }
 
-func (m *updateVisitor) Enter(n ast.Node) (node ast.Node, skipChildren bool) {
+func (m *updateVisitor) Enter(n ast.Node) (ast.Node, bool) {
 	return n, true
 }
 
-func (m *updateVisitor) Leave(n ast.Node) (node ast.Node, ok bool) {
-	node = n
+func (m *updateVisitor) Leave(n ast.Node) (ast.Node, bool) {
+	node := n
 	return node, true
 }
--- pkg/datasource/sql/exec/at/multi_update_excutor_test.go
+++ pkg/datasource/sql/exec/at/multi_update_excutor_test.go
@@ -1,8 +1,26 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package at
 
 import (
 	"database/sql/driver"
-	"github.com/arana-db/parser/ast"
+	"testing"
+
 	"github.com/seata/seata-go/pkg/datasource/sql/datasource"
 	"github.com/seata/seata-go/pkg/datasource/sql/datasource/mysql"
 	"github.com/seata/seata-go/pkg/datasource/sql/exec"
@@ -11,7 +29,6 @@ import (
 	"github.com/seata/seata-go/pkg/datasource/sql/undo"
 	"github.com/seata/seata-go/pkg/datasource/sql/util"
 	"github.com/stretchr/testify/assert"
-	"testing"
 )
 
 func TestBuildSelectSQLByMultiUpdate(t *testing.T) {
@@ -52,16 +69,16 @@ func TestBuildSelectSQLByMultiUpdate(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			c, err := parser.DoParser(tt.sourceQuery)
 			assert.Nil(t, err)
-			var updateStmts []*ast.UpdateStmt
-			for _, v := range c.MultiStmt {
-				updateStmts = append(updateStmts, v.UpdateStmt)
-			}
-			executor := NewMultiUpdateExecutor(c, &types.ExecContext{Values: tt.sourceQueryArgs, NamedValues: util.ValueToNamedValue(tt.sourceQueryArgs)}, []exec.SQLHook{})
+			//updateStmts := make([]*ast.UpdateStmt, 0, len(c.MultiStmt))
+			//for _, v := range c.MultiStmt {
+			//	updateStmts = append(updateStmts, v.UpdateStmt)
+			//}
+			executor := NewMultiUpdateExecutor(c, &types.ExecContext{NamedValues: util.ValueToNamedValue(tt.sourceQueryArgs)}, []exec.SQLHook{})
 
-			query, args, err := executor.buildBeforeImageSQL(updateStmts, tt.sourceQueryArgs, types.TableMeta{})
+			query, args, err := executor.buildBeforeImageSQL(util.ValueToNamedValue(tt.sourceQueryArgs), &types.TableMeta{})
 			assert.NoError(t, err)
 			assert.Equal(t, tt.expectQuery, query)
-			assert.Equal(t, tt.expectQueryArgs, args)
+			assert.Equal(t, tt.expectQueryArgs, util.NamedValueToValue(args))
 		})
 	}
 
@@ -70,13 +87,13 @@ func TestBuildSelectSQLByMultiUpdate(t *testing.T) {
 	sourceQueryArgs := []driver.Value{"Jack", 1, 10, 20, 17, "Beijing", "Guangzhou", 18, 2, "Jack2", 1, 10, 20, 17, "Beijing", "Guangzhou", 18, 2}
 	c, err := parser.DoParser(sourceQuery)
 	assert.NoError(t, err)
-	var updateStmts []*ast.UpdateStmt
-	for _, v := range c.MultiStmt {
-		updateStmts = append(updateStmts, v.UpdateStmt)
-	}
+	//updateStmts := make([]*ast.UpdateStmt, 0, len(c.MultiStmt))
+	//for _, v := range c.MultiStmt {
+	//	updateStmts = append(updateStmts, v.UpdateStmt)
+	//}
 
-	executor := NewMultiUpdateExecutor(c, &types.ExecContext{Values: sourceQueryArgs, NamedValues: util.ValueToNamedValue(sourceQueryArgs)}, []exec.SQLHook{})
-	_, _, err = executor.buildBeforeImageSQL(updateStmts, sourceQueryArgs, types.TableMeta{})
+	executor := NewMultiUpdateExecutor(c, &types.ExecContext{NamedValues: util.ValueToNamedValue(sourceQueryArgs)}, []exec.SQLHook{})
+	_, _, err = executor.buildBeforeImageSQL(util.ValueToNamedValue(sourceQueryArgs), &types.TableMeta{})
 	assert.Error(t, err)
 	assert.Equal(t, err.Error(), "multi update SQL with orderBy condition is not support yet")
 }
@@ -124,16 +141,16 @@ func TestBuildSelectSQLByMultiUpdateAllColumns(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			c, err := parser.DoParser(tt.sourceQuery)
 			assert.Nil(t, err)
-			var updateStmts []*ast.UpdateStmt
-			for _, v := range c.MultiStmt {
-				updateStmts = append(updateStmts, v.UpdateStmt)
-			}
+			//updateStmts := make([]*ast.UpdateStmt, 0, len(c.MultiStmt))
+			//for _, v := range c.MultiStmt {
+			//	updateStmts = append(updateStmts, v.UpdateStmt)
+			//}
 			executor := NewMultiUpdateExecutor(c, &types.ExecContext{Values: tt.sourceQueryArgs, NamedValues: util.ValueToNamedValue(tt.sourceQueryArgs)}, []exec.SQLHook{})
 
-			query, args, err := executor.buildBeforeImageSQL(updateStmts, tt.sourceQueryArgs, meta)
+			query, args, err := executor.buildBeforeImageSQL(util.ValueToNamedValue(tt.sourceQueryArgs), &meta)
 			assert.NoError(t, err)
 			assert.Equal(t, tt.expectQuery, query)
-			assert.Equal(t, tt.expectQueryArgs, args)
+			assert.Equal(t, tt.expectQueryArgs, util.NamedValueToValue(args))
 		})
 	}
 
@@ -142,13 +159,13 @@ func TestBuildSelectSQLByMultiUpdateAllColumns(t *testing.T) {
 	sourceQueryArgs := []driver.Value{"Jack", 1, 10, 20, 17, "Beijing", "Guangzhou", 18, 2, "Jack2", 1, 10, 20, 17, "Beijing", "Guangzhou", 18, 2}
 	c, err := parser.DoParser(sourceQuery)
 	assert.NoError(t, err)
-	var updateStmts []*ast.UpdateStmt
-	for _, v := range c.MultiStmt {
-		updateStmts = append(updateStmts, v.UpdateStmt)
-	}
+	//updateStmts := make([]*ast.UpdateStmt, 0, len(c.MultiStmt))
+	//for _, v := range c.MultiStmt {
+	//	updateStmts = append(updateStmts, v.UpdateStmt)
+	//}
 
 	executor := NewMultiUpdateExecutor(c, &types.ExecContext{Values: sourceQueryArgs, NamedValues: util.ValueToNamedValue(sourceQueryArgs)}, []exec.SQLHook{})
-	_, _, err = executor.buildBeforeImageSQL(updateStmts, sourceQueryArgs, meta)
+	_, _, err = executor.buildBeforeImageSQL(util.ValueToNamedValue(sourceQueryArgs), &meta)
 	assert.Error(t, err)
 	assert.Equal(t, err.Error(), "multi update SQL with orderBy condition is not support yet")
 }
