1f550cf187c0a394510644dbeb982a43c34c75f1
Luky116Liuyuecai <luky116@126.com>
2023-03-01 08:09:14 +0800

    fix at bug

--- pkg/datasource/sql/exec/at/multi_executor.go
+++ pkg/datasource/sql/exec/at/multi_executor.go
@@ -19,7 +19,6 @@ package at
 
 import (
 	"context"
-	"errors"
 	"fmt"
 
 	"github.com/seata/seata-go/pkg/datasource/sql/exec"
@@ -60,17 +59,10 @@ func (m *multiExecutor) ExecContext(ctx context.Context, f exec.CallbackWithName
 		return nil, err
 	}
 
-	if len(afterImages) != len(beforeImages) {
-		return nil, errors.New("Before image size is not equaled to after image size, probably because you updated the primary keys.")
-	}
-
-	for i, afterImage := range afterImages {
-		beforeImage := beforeImages[i]
-		if len(beforeImage.Rows) != len(afterImage.Rows) {
-			return nil, errors.New("Before image size is not equaled to after image size, probably because you updated the primary keys.")
-		}
-
+	for _, beforeImage := range beforeImages {
 		m.execContext.TxCtx.RoundImages.AppendBeofreImage(beforeImage)
+	}
+	for _, afterImage := range afterImages {
 		m.execContext.TxCtx.RoundImages.AppendAfterImage(afterImage)
 	}
 
