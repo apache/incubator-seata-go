1938743bba41b6b33760568f625eb6833584a394
唯德 <qinkai.qin@alibaba-inc.com>
2023-03-24 10:26:44 +0800

    reactor:support nacos registry

--- cmd/start.go
+++ cmd/start.go
@@ -17,46 +17,5 @@
 
 package main
 
-import (
-	"github.com/seata/seata-go/pkg/client"
-	"github.com/seata/seata-go/pkg/registry"
-	"net"
-	"time"
-)
-
 func main() {
-	register()
-	//deregister()
-	//getService()
-	//subscribe()
-}
-
-func initConfig() (registry.RegistryService, error) {
-	path := &client.LoadPath("/Users/ali/Desktop/GO/vader/seata-go/testdata/conf/seatago.yml").RegistryConfig
-	return registry.GetRegistry(path)
-}
-func register() {
-	register, _ := initConfig()
-	address := net.TCPAddr{
-		IP:   net.IPv4zero,
-		Port: 9001,
-	}
-	register.RegisterServiceInstance(address)
-	//for i := 0; i < 10; i++ {
-	time.Sleep(1000 * time.Second)
-	//}
-}
-
-func deregister() {
-	register, _ := initConfig()
-	address := net.TCPAddr{
-		IP:   net.IPv4zero,
-		Port: 9001,
-	}
-	register.DeRegisterServiceInstance(address)
-}
-
-func subscribe() {
-	register, _ := initConfig()
-	register.Subscribe("DEFAULT", "GroupName:SEATA_GROUP")
 }
--- pkg/registry/registry_test.go
+++ pkg/registry/registry_test.go
@@ -7,15 +7,32 @@ import (
 	"time"
 )
 
-func TestGetRegistry(t *testing.T) {
-	cfg := client.LoadPath("/Users/ali/Desktop/GO/vader/seata-go/testdata/conf/seatago.yml")
-	register, _ := GetRegistry(&cfg.RegistryConfig)
+func initConfig() (RegistryService, error) {
+	path := &client.LoadPath("/Users/ali/Desktop/GO/vader/seata-go/testdata/conf/seatago.yml").RegistryConfig
+	return GetRegistry(path)
+}
+func TestRegister(*testing.T) {
+	register, _ := initConfig()
 	address := net.TCPAddr{
 		IP:   net.IPv4zero,
-		Port: 9002,
+		Port: 9001,
 	}
 	register.RegisterServiceInstance(address)
 	//for i := 0; i < 10; i++ {
-	time.Sleep(3 * time.Second)
+	time.Sleep(1000 * time.Second)
 	//}
+}
+
+func TestDeregister(*testing.T) {
+	register, _ := initConfig()
+	address := net.TCPAddr{
+		IP:   net.IPv4zero,
+		Port: 9001,
+	}
+	register.DeRegisterServiceInstance(address)
+}
+
+func TestSubscribe(*testing.T) {
+	register, _ := initConfig()
+	register.Subscribe("DEFAULT", "GroupName:SEATA_GROUP")
 }
