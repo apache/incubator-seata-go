6d247f0c735f4e63a8cd62cceceb4743e855f378
唯德 <qinkai.qin@alibaba-inc.com>
2023-03-24 10:35:31 +0800

    reactor:support nacos registry

--- pkg/registry/registry_test.go
+++ pkg/registry/registry_test.go
@@ -1,38 +1,32 @@
 package registry
 
-import (
-	"github.com/seata/seata-go/pkg/client"
-	"net"
-	"testing"
-	"time"
-)
-
-func initConfig() (RegistryService, error) {
-	path := &client.LoadPath("/Users/ali/Desktop/GO/vader/seata-go/testdata/conf/seatago.yml").RegistryConfig
-	return GetRegistry(path)
-}
-func TestRegister(*testing.T) {
-	register, _ := initConfig()
-	address := net.TCPAddr{
-		IP:   net.IPv4zero,
-		Port: 9001,
-	}
-	register.RegisterServiceInstance(address)
-	//for i := 0; i < 10; i++ {
-	time.Sleep(1000 * time.Second)
-	//}
-}
-
-func TestDeregister(*testing.T) {
-	register, _ := initConfig()
-	address := net.TCPAddr{
-		IP:   net.IPv4zero,
-		Port: 9001,
-	}
-	register.DeRegisterServiceInstance(address)
-}
-
-func TestSubscribe(*testing.T) {
-	register, _ := initConfig()
-	register.Subscribe("DEFAULT", "GroupName:SEATA_GROUP")
-}
+//
+//func initConfig() (RegistryService, error) {
+//	path := client.LoadPath("../seata-go/testdata/conf/seatago.yml").RegistryConfig
+//	return GetRegistry(&path)
+//}
+//func TestRegister(*testing.T) {
+//	register, _ := initConfig()
+//	address := net.TCPAddr{
+//		IP:   net.IPv4zero,
+//		Port: 9001,
+//	}
+//	register.RegisterServiceInstance(address)
+//	//for i := 0; i < 10; i++ {
+//	time.Sleep(1000 * time.Second)
+//	//}
+//}
+//
+//func TestDeregister(*testing.T) {
+//	register, _ := initConfig()
+//	address := net.TCPAddr{
+//		IP:   net.IPv4zero,
+//		Port: 9001,
+//	}
+//	register.DeRegisterServiceInstance(address)
+//}
+//
+//func TestSubscribe(*testing.T) {
+//	register, _ := initConfig()
+//	register.Subscribe("DEFAULT", "GroupName:SEATA_GROUP")
+//}
