58fc3e13dffa176082f39a3a989b20b2752c47e0
wt_better <m18091785089@163.com>
2023-12-23 23:01:17 +0800

    feature: init saga framework (#635)
    init saga framework

---
+++ pkg/saga/statemachine/engine/invoker/invoker.go
@@ -0,0 +1,14 @@
+package invoker
+
+type ScriptInvokerManager interface {
+}
+
+type ScriptInvoker interface {
+}
+
+type ServiceInvokerManager interface {
+}
+
+type ServiceInvoker interface {
+	Invoke()
+}
