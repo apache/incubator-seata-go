1e0fdbf4c45ac6b7ffafdc42a7e3f12510995907
Rulin Tang <smiletrl@outlook.com>
2024-02-28 20:02:08 +0800

    fix config center

--- pkg/client/config.go
+++ pkg/client/config.go
@@ -85,7 +85,7 @@ type Config struct {
 	TransportConfig   remoteConfig.TransportConfig `yaml:"transport" json:"transport" koanf:"transport"`
 	ServiceConfig     discovery.ServiceConfig      `yaml:"service" json:"service" koanf:"service"`
 	RegistryConfig    discovery.RegistryConfig     `yaml:"registry" json:"registry" koanf:"registry"`
-	ConfigConfig      discovery.ConfigConfig       `yaml:"config" json:"config" koanf:"config"`
+	ConfigConfig      discovery.ConfigCenterConfig `yaml:"config" json:"config" koanf:"config"`
 }
 
 func (c *Config) RegisterFlags(f *flag.FlagSet) {
