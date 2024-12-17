b4dc605c820a9ef5bedf8c4b581fea43484ef10c
marsevilspirit <marsevilspirit@gmail.com>
2024-12-18 03:19:56 +0800

    add func task state

---
+++ pkg/saga/statemachine/engine/invoker/func_invoker.go
@@ -0,0 +1,114 @@
+package invoker
+
+import (
+	"context"
+	"errors"
+	"reflect"
+	"sync"
+
+	"github.com/seata/seata-go/pkg/saga/statemachine/statelang/state"
+	"github.com/seata/seata-go/pkg/util/log"
+)
+
+type FuncInvoker struct {
+	services        map[string]FuncService
+	ServicesMapLock sync.Mutex
+}
+
+func NewFuncInvoker() *FuncInvoker {
+	return &FuncInvoker{
+		services: make(map[string]FuncService),
+	}
+}
+
+func (f *FuncInvoker) RegisterService(serviceName string, service FuncService) {
+	f.ServicesMapLock.Lock()
+	defer f.ServicesMapLock.Unlock()
+	f.services[serviceName] = service
+}
+
+func (f *FuncInvoker) GetService(serviceName string) FuncService {
+	f.ServicesMapLock.Lock()
+	defer f.ServicesMapLock.Unlock()
+	return f.services[serviceName]
+}
+
+func (f *FuncInvoker) Invoke(ctx context.Context, input []any, service state.ServiceTaskState) (output []reflect.Value, err error) {
+	serviceTaskStateImpl := service.(*state.ServiceTaskStateImpl)
+	FuncService := f.GetService(serviceTaskStateImpl.ServiceName())
+	if FuncService == nil {
+		return nil, errors.New("no func service " + serviceTaskStateImpl.ServiceName() + " for service task state")
+	}
+
+	if serviceTaskStateImpl.IsAsync() {
+		go func() {
+			_, err := FuncService.CallMethod(serviceTaskStateImpl, input)
+			if err != nil {
+				log.Errorf("invoke Service[%s].%s failed, err is %s", serviceTaskStateImpl.ServiceName(), serviceTaskStateImpl.ServiceMethod(), err.Error())
+			}
+		}()
+		return nil, nil
+	} else {
+		return FuncService.CallMethod(serviceTaskStateImpl, input)
+	}
+}
+
+func (f *FuncInvoker) Close(ctx context.Context) error {
+	return nil
+}
+
+type FuncService interface {
+	CallMethod(ServiceTaskStateImpl *state.ServiceTaskStateImpl, input []any) ([]reflect.Value, error)
+}
+
+type FuncServiceImpl struct {
+	serviceName string
+	method      any
+	methodLock  sync.Mutex
+}
+
+func NewFuncClient(serviceName string, method any) *FuncServiceImpl {
+	return &FuncServiceImpl{
+		serviceName: serviceName,
+		method:      method,
+	}
+}
+
+func (f *FuncServiceImpl) CallMethod(serviceTaskStateImpl *state.ServiceTaskStateImpl, input []any) ([]reflect.Value, error) {
+	if serviceTaskStateImpl.Method() == nil {
+		err := f.initMethod(serviceTaskStateImpl)
+		if err != nil {
+			return nil, err
+		}
+	}
+	method := serviceTaskStateImpl.Method()
+
+	args := make([]reflect.Value, 0, len(input))
+	for _, arg := range input {
+		args = append(args, reflect.ValueOf(arg))
+	}
+
+	return method.Call(args), nil
+}
+
+func (f *FuncServiceImpl) initMethod(serviceTaskStateImpl *state.ServiceTaskStateImpl) error {
+	methodName := serviceTaskStateImpl.ServiceMethod()
+	f.methodLock.Lock()
+	defer f.methodLock.Unlock()
+	methodValue := reflect.ValueOf(f.method)
+	if methodValue.IsZero() {
+		return errors.New("invalid method when func call, serviceName: " + f.serviceName)
+	}
+
+	if methodValue.Kind() == reflect.Func {
+		serviceTaskStateImpl.SetMethod(&methodValue)
+		return nil
+	}
+
+	method := methodValue.MethodByName(methodName)
+	if method.IsZero() {
+		return errors.New("invalid method name when func call, serviceName: " + f.serviceName + ", methodName: " + methodName)
+	}
+	serviceTaskStateImpl.SetMethod(&method)
+	return nil
+}
---
+++ pkg/saga/statemachine/engine/invoker/func_invoker_test.go
@@ -0,0 +1,87 @@
+package invoker
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/seata/seata-go/pkg/saga/statemachine/statelang/state"
+)
+
+type MockFuncClientImpl struct {
+	FuncServiceImpl
+}
+
+// struct's method test
+type mockFuncImpl struct {
+}
+
+func (m *mockFuncImpl) SayHelloRight(word string) string {
+	fmt.Println("invoke right")
+	return word
+}
+
+func TestFuncInvokerInvokeStructSucceed(t *testing.T) {
+	ctx := context.Background()
+	invoker := newFuncServiceStructInvoker()
+	values, err := invoker.Invoke(ctx, []any{"hello"}, newFuncHelloServiceTaskState())
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if values == nil || len(values) == 0 {
+		t.Error("no value in values")
+		return
+	}
+	if values[0].Interface().(string) != "hello" {
+		t.Errorf("expect hello, but got %s", values[0].Interface())
+	}
+}
+
+func newFuncServiceStructInvoker() ServiceInvoker {
+	mockFuncInvoker := NewFuncInvoker()
+	mockFuncService := &mockFuncImpl{}
+	mockService := NewFuncClient("hello", mockFuncService)
+	mockFuncInvoker.RegisterService("hello", mockService)
+	return mockFuncInvoker
+}
+
+// method test
+func SayHelloRight(word string) string {
+	return word
+}
+
+func TestFuncInvokerInvokeMethodSucceed(t *testing.T) {
+	ctx := context.Background()
+	invoker := newFuncServiceMethodInvoker()
+	values, err := invoker.Invoke(ctx, []any{"hello"}, newFuncHelloServiceTaskState())
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if values == nil || len(values) == 0 {
+		t.Error("no value in values")
+		return
+	}
+	if values[0].Interface().(string) != "hello" {
+		t.Errorf("expect hello, but got %s", values[0].Interface())
+	}
+}
+
+func newFuncServiceMethodInvoker() ServiceInvoker {
+	mockFuncInvoker := NewFuncInvoker()
+	mockFuncMethod := SayHelloRight
+	mockService := NewFuncClient("hello", mockFuncMethod)
+	mockFuncInvoker.RegisterService("hello", mockService)
+	return mockFuncInvoker
+}
+
+func newFuncHelloServiceTaskState() state.ServiceTaskState {
+	serviceTaskStateImpl := state.NewServiceTaskStateImpl()
+	serviceTaskStateImpl.SetName("hello")
+	serviceTaskStateImpl.SetIsAsync(false)
+	serviceTaskStateImpl.SetServiceName("hello")
+	serviceTaskStateImpl.SetServiceType("func")
+	serviceTaskStateImpl.SetServiceMethod("SayHelloRight")
+	return serviceTaskStateImpl
+}
