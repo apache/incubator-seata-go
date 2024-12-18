3980494c346f6da949d695a5d472aaf995c167ad
marsevilspirit <marsevilspirit@gmail.com>
2024-12-18 21:46:06 +0800

    update

--- pkg/saga/statemachine/engine/invoker/func_invoker_test.go
+++ pkg/saga/statemachine/engine/invoker/func_invoker_test.go
@@ -9,11 +9,7 @@ import (
 	"github.com/seata/seata-go/pkg/saga/statemachine/statelang/state"
 )
 
-type MockFuncClientImpl struct {
-	FuncServiceImpl
-}
-
-// struct's method test
+// struct's method mock implementation
 type mockFuncImpl struct {
 	invokeCount int
 }
@@ -34,56 +30,63 @@ func (m *mockFuncImpl) SayHelloRightLater(word string, delay int) (string, error
 	return "", errors.New("invoke failed")
 }
 
-func TestFuncInvokerInvokeStructSucceed(t *testing.T) {
-	ctx := context.Background()
-	invoker := newFuncServiceStructInvoker()
-	values, err := invoker.Invoke(ctx, []any{"hello"}, newFuncHelloServiceTaskState())
-	if err != nil {
-		t.Error(err)
-		return
+func TestFuncInvokerInvokeStruct(t *testing.T) {
+	tests := []struct {
+		name      string
+		input     []any
+		taskState state.ServiceTaskState
+		expectVal string
+		expectErr error
+	}{
+		{
+			name:      "Invoke Struct Succeed",
+			input:     []any{"hello"},
+			taskState: newFuncHelloServiceTaskState(),
+			expectVal: "hello",
+			expectErr: nil,
+		},
+		{
+			name:      "Invoke Struct Succeed In Retry",
+			input:     []any{"hello", 2},
+			taskState: newFuncHelloServiceTaskStateWithRetry(),
+			expectVal: "hello",
+			expectErr: nil,
+		},
+		{
+			name:      "Invoke Struct Failed In Retry",
+			input:     []any{"hello", 5},
+			taskState: newFuncHelloServiceTaskStateWithRetry(),
+			expectVal: "",
+			expectErr: errors.New("expect error, but got nil"),
+		},
 	}
-	if values == nil || len(values) == 0 {
-		t.Error("no value in values")
-		return
-	}
-	if values[0].Interface().(string) != "hello" {
-		t.Errorf("expect hello, but got %s", values[0].Interface())
-	}
-	if _, ok := values[1].Interface().(error); ok {
-		t.Errorf("expect nil, but got %s", values[1].Interface())
-	}
-}
 
-func TestFuncInvokerInvokeStructSucceedInRetry(t *testing.T) {
 	ctx := context.Background()
-	invoker := newFuncServiceStructInvoker()
-	values, err := invoker.Invoke(ctx, []any{"hello", 2}, newFuncHelloServiceTaskStateWithRetry())
-	if err != nil {
-		t.Error(err)
-		return
-	}
-	if values == nil || len(values) == 0 {
-		t.Error("no value in values")
-		return
-	}
-	if values[0].Interface().(string) != "hello" {
-		t.Errorf("expect hello, but got %s", values[0].Interface())
-	}
-	if _, ok := values[1].Interface().(error); ok {
-		t.Errorf("expect nil, but got %s", values[1].Interface())
-	}
-}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			invoker := newFuncServiceStructInvoker()
+			values, err := invoker.Invoke(ctx, tt.input, tt.taskState)
 
-func TestFuncInvokerInvokeStructFailedInRetry(t *testing.T) {
-	ctx := context.Background()
-	invoker := newFuncServiceStructInvoker()
-	values, err := invoker.Invoke(ctx, []any{"hello", 5}, newFuncHelloServiceTaskStateWithRetry())
-	if err == nil {
-		t.Error("expect error, but got nil")
-		return
-	}
-	if values != nil {
-		t.Errorf("expect nil, but got %v", values)
+			if tt.expectErr != nil && err == nil {
+				t.Error(tt.expectErr)
+				return
+			} else if tt.expectErr == nil && err != nil {
+				t.Error(err)
+				return
+			}
+
+			if values == nil || len(values) == 0 {
+				t.Error("no value in values")
+				return
+			}
+
+			if values[0].Interface().(string) != tt.expectVal {
+				t.Errorf("expected %s, but got %s", tt.expectVal, values[0].Interface())
+			}
+			if _, ok := values[1].Interface().(error); ok {
+				t.Errorf("expected nil, but got %s", values[1].Interface())
+			}
+		})
 	}
 }
 
