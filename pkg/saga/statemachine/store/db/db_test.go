129e63cdb694c597f75e5f27ce27004710714077
1kasa <3049907027@qq.com>
2025-03-29 18:34:42 +0800

    fix: Wrong commit

--- pkg/saga/statemachine/store/db/db_test.go
+++ pkg/saga/statemachine/store/db/db_test.go
@@ -32,7 +32,7 @@ func prepareDB() {
 	oncePrepareDB.Do(func() {
 		var err error
 		db, err = sql.Open("sqlite3", ":memory:")
-		query_, err := os.ReadFile("../../../../../testdata/sql/saga/sqlite_init.sql")
+		query_, err := os.ReadFile("testdata/sql/saga/sqlite_init.sql")
 		initScript := string(query_)
 		if err != nil {
 			panic(err)
