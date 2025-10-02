6f69be8ff8047d1feafd33f82949f969641b9841
Wangzy <wangziyuan455@Gmail.com>
2025-10-02 20:29:04 +0800

    fix bugs detected by doubao

--- pkg/remoting/getty/getty_client.go
+++ pkg/remoting/getty/getty_client.go
@@ -18,29 +18,16 @@
 package getty
 
 import (
-	"context"
 	"fmt"
-	"net"
-	"strconv"
 	"sync"
-	"time"
 
-	getty "github.com/apache/dubbo-getty"
-	gxsync "github.com/dubbogo/gost/sync"
 	gxtime "github.com/dubbogo/gost/time"
 	"go.uber.org/atomic"
 
 	"seata.apache.org/seata-go/pkg/protocol/codec"
 	"seata.apache.org/seata-go/pkg/protocol/message"
-	"seata.apache.org/seata-go/pkg/remoting/config"
-	"seata.apache.org/seata-go/pkg/util/backoff"
 	"seata.apache.org/seata-go/pkg/util/log"
 )
-
-func init() {
-	c := config.GetGettyConfig()
-	initSessionManager(c)
-}
 
 var (
 	gettyRemotingClient     *GettyRemotingClient
@@ -117,64 +104,6 @@ func (g *GettyRemotingClient) syncCallback(reqMsg message.RpcMessage, respMsg *m
 	case <-respMsg.Done:
 		return respMsg.Response, respMsg.Err
 	}
-}
-
-func (client *GettyRemotingClient) reconnectWithBackoff(session getty.Session, maxRetries int) {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	cfg := backoff.Config{
-		MinBackoff: 1 * time.Second,
-		MaxBackoff: 60 * time.Second,
-		MaxRetries: maxRetries,
-	}
-	backoffInstance := backoff.New(ctx, cfg)
-
-	log.Infof("Starting reconnection attempts with backoff for session: %s", session.Stat())
-
-	for backoffInstance.Ongoing() {
-		backoffInstance.Wait()
-
-		log.Infof("Reconnection attempt %d for session: %s", backoffInstance.NumRetries(), session.Stat())
-		//retry
-		if err := client.reconnectToServer(session); err == nil {
-			return
-		}
-	}
-
-	if err := backoffInstance.Err(); err != nil {
-		log.Errorf("Reconnection failed after %d attempts: %v", backoffInstance.NumRetries(), err)
-	}
-}
-
-func (client *GettyRemotingClient) reconnectToServer(session getty.Session) error {
-
-	remoteAddr := session.RemoteAddr()
-
-	host, port, err := net.SplitHostPort(remoteAddr)
-	if err != nil {
-		log.Errorf("Failed to parse remote address %s: %v", remoteAddr, err)
-		return err
-	}
-
-	_, err = strconv.Atoi(port)
-	if err != nil {
-		log.Errorf("Failed to parse port %s: %v", port, err)
-		return err
-	}
-
-	session.Close()
-
-	gettyClient := getty.NewTCPClient(
-		getty.WithServerAddress(net.JoinHostPort(host, port)),
-		getty.WithConnectionNumber(1),
-		getty.WithReconnectInterval(GetSessionManager().gettyConf.ReconnectInterval),
-		getty.WithClientTaskPool(gxsync.NewTaskPoolSimple(0)),
-	)
-
-	go gettyClient.RunEventLoop(GetSessionManager().newSession)
-
-	log.Infof("Reconnection initiated for server %s", remoteAddr)
-	return nil
 }
 
 func (client *GettyRemotingClient) GetMergedMessage(msgID int32) *message.MergedWarpMessage {
