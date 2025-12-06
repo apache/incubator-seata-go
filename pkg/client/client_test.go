a255f312ce9a7134b699a6fd012b57eabe209d3d
zfCode <zfcode@gmail.com>
2025-12-06 18:35:53 +0800

    fix struct

--- pkg/client/client_test.go
+++ pkg/client/client_test.go
@@ -22,6 +22,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
 	"seata.apache.org/seata-go/pkg/tm"
 )
 
--- pkg/datasource/sql/undo/builder/basic_undo_log_builder.go
+++ pkg/datasource/sql/undo/builder/basic_undo_log_builder.go
@@ -22,12 +22,14 @@ import (
 	"database/sql"
 	"database/sql/driver"
 	"fmt"
+	"io"
+	"strings"
+
 	"github.com/arana-db/parser/ast"
 	"github.com/arana-db/parser/test_driver"
 	gxsort "github.com/dubbogo/gost/sort"
-	"io"
+
 	"seata.apache.org/seata-go/pkg/datasource/sql/util"
-	"strings"
 
 	"seata.apache.org/seata-go/pkg/datasource/sql/types"
 )
