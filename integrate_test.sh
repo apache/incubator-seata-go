9c8fc7905d898c060971550ef0260cbbd76664e9
Oxidaner <18622412361@163.com>
2025-12-24 09:21:40 +0800

    fix: CI go mod replace path (add /v2 suffix)

--- integrate_test.sh
+++ integrate_test.sh
@@ -38,9 +38,11 @@ echo "github pull request head branch -> ${GITHUB_HEAD_REF}"
 echo "use seata-go-samples $3 branch for integration testing"
 git clone https://github.com/apache/incubator-seata-go-samples samples && cd samples
 
+go clean -modcache
+
 # update seata-go to current commit id
 
-go mod edit -replace=seata.apache.org/seata-go/v2=github.com/"$1"@"$2"
+go mod edit -replace=seata.apache.org/seata-go/v2=github.com/"$1"@"$2"/v2
 
 go mod tidy
 
