d55f122266de7285efebb36e68b8e9039bd93575
Oxidaner <18622412361@163.com>
2025-12-24 12:52:52 +0800

    fix: remove coverage from git and add to .gitignore

--- integrate_test.sh
+++ integrate_test.sh
@@ -40,9 +40,10 @@ git clone https://github.com/apache/incubator-seata-go-samples samples && cd sam
 
 
 # update seata-go to current commit id
+go mod edit -replace=seata.apache.org/seata-go=seata.apache.org/seata-go/v2
+go mod edit -replace=seata.apache.org/seata-go/v2=github.com/"$1"/v2@"$2"
 
-go mod edit -replace=seata.apache.org/seata-go=github.com/"$1"@"$2"
-
+go clean -modcache
 go mod tidy
 
 # start integrate test
