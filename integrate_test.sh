ce4e7ddddcf261b7dc02d8316b75f7d6befc299a
Oxidaner <18622412361@163.com>
2025-12-25 13:32:50 +0800

    fix: repair integrate_test

--- integrate_test.sh
+++ integrate_test.sh
@@ -38,12 +38,11 @@ echo "github pull request head branch -> ${GITHUB_HEAD_REF}"
 echo "use seata-go-samples $3 branch for integration testing"
 git clone https://github.com/apache/incubator-seata-go-samples samples && cd samples
 
-
 # update seata-go to current commit id
-go mod edit -replace=seata.apache.org/seata-go=seata.apache.org/seata-go/v2
-go mod edit -replace=seata.apache.org/seata-go/v2=github.com/"$1"/v2@"$2"
 
-go clean -modcache
+go mod edit -replace=seata.apache.org/seata-go=github.com/"$1"@"$2"
+go mod edit -replace=seata.apache.org/seata-go/v2=github.com/"$1"@"$2"
+
 go mod tidy
 
 # start integrate test
