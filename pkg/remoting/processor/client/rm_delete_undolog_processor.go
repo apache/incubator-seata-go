eeb6e825ce227610e7efbe6cc90626bac3e2dfb2
everfid-ever <166227111+everfid-ever@users.noreply.github.com>
2026-03-29 02:50:28 +0800

    feat: realize undo_log deletion logic

--- pkg/remoting/processor/client/rm_delete_undolog_processor.go
+++ pkg/remoting/processor/client/rm_delete_undolog_processor.go
@@ -1,34 +1,23 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package client
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
+	"time"
 
+	"seata.apache.org/seata-go/v2/pkg/datasource/sql/types"
+	"seata.apache.org/seata-go/v2/pkg/datasource/sql/undo"
+	"seata.apache.org/seata-go/v2/pkg/protocol/branch"
 	"seata.apache.org/seata-go/v2/pkg/protocol/message"
 	"seata.apache.org/seata-go/v2/pkg/remoting/getty"
+	"seata.apache.org/seata-go/v2/pkg/rm"
 	"seata.apache.org/seata-go/v2/pkg/util/log"
 )
 
 func initDeleteUndoLog() {
-	rmDeleteUndoLogProcessor := &rmDeleteUndoLogProcessor{}
-	getty.GetGettyClientHandlerInstance().RegisterProcessor(message.MessageTypeRmDeleteUndolog, rmDeleteUndoLogProcessor)
+	getty.GetGettyClientHandlerInstance().RegisterProcessor(
+		message.MessageTypeRmDeleteUndolog, &rmDeleteUndoLogProcessor{})
 }
 
 type rmDeleteUndoLogProcessor struct{}
@@ -39,12 +28,98 @@ func (r *rmDeleteUndoLogProcessor) Process(ctx context.Context, rpcMessage messa
 		return fmt.Errorf("invalid message body type: %T", rpcMessage.Body)
 	}
 
-	log.Infof("Received undo log delete request: resourceId=%s, saveDays=%d, branchType=%v",
-		req.ResourceId, req.SaveDays, req.BranchType)
+	if req.BranchType != branch.BranchTypeAT {
+		log.Infof("skip undo log delete for non-AT branch type: %v", req.BranchType)
+		return nil
+	}
 
-	// TODO: Implement actual undo log deletion logic here
-	// This should delete undo log records older than saveDays for the specified resourceId and branchType
+	log.Infof("received undo log delete request: resourceId=%s, saveDays=%d",
+		req.ResourceId, req.SaveDays)
 
-	// Note: The TC server sends this message as a one-way notification, no response is required
+	if err := r.deleteExpiredUndoLog(ctx, req); err != nil {
+		log.Errorf("failed to delete expired undo log: resourceId=%s, saveDays=%d, err=%v",
+			req.ResourceId, req.SaveDays, err)
+	}
+
 	return nil
+}
+
+type dbResource interface {
+	GetDB() *sql.DB
+	GetDbType() types.DBType
+}
+
+func (r *rmDeleteUndoLogProcessor) deleteExpiredUndoLog(ctx context.Context, req message.UndoLogDeleteRequest) error {
+	resMgr, err := safeGetResourceManager(req.BranchType)
+	if err != nil {
+		return err
+	}
+
+	val, ok := resMgr.GetCachedResources().Load(req.ResourceId)
+	if !ok {
+		return fmt.Errorf("resource not found in cache: %s", req.ResourceId)
+	}
+
+	res, ok := val.(dbResource)
+	if !ok {
+		return fmt.Errorf("resource %s does not implement dbResource interface", req.ResourceId)
+	}
+
+	conn, err := res.GetDB().Conn(ctx)
+	if err != nil {
+		return fmt.Errorf("get conn: %w", err)
+	}
+	defer conn.Close()
+
+	undoMgr, err := undo.GetUndoLogManager(res.GetDbType())
+	if err != nil {
+		return fmt.Errorf("get undo log manager for dbType %v: %w", res.GetDbType(), err)
+	}
+
+	exists, err := undoMgr.HasUndoLogTable(ctx, conn)
+	if err != nil {
+		return fmt.Errorf("check undo log table: %w", err)
+	}
+	if !exists {
+		log.Infof("undo_log table not exist, skip: resourceId=%s", req.ResourceId)
+		return nil
+	}
+
+	before := time.Now().AddDate(0, 0, -int(req.SaveDays))
+	return r.batchDeleteByLogCreated(ctx, conn, before)
+}
+
+func (r *rmDeleteUndoLogProcessor) batchDeleteByLogCreated(ctx context.Context, conn *sql.Conn, before time.Time) error {
+	undoLogTable := undo.UndoConfig.LogTable
+	if undoLogTable == "" {
+		undoLogTable = "undo_log"
+	}
+
+	deleteSQL := fmt.Sprintf("DELETE FROM %s WHERE log_created <= ? LIMIT 1000", undoLogTable)
+
+	totalAffected := int64(0)
+	for {
+		result, err := conn.ExecContext(ctx, deleteSQL, before)
+		if err != nil {
+			return fmt.Errorf("exec delete: %w", err)
+		}
+		affected, _ := result.RowsAffected()
+		totalAffected += affected
+		if affected < 1000 {
+			break
+		}
+	}
+
+	log.Infof("deleted expired undo log: before=%v, totalAffected=%d", before, totalAffected)
+	return nil
+}
+
+func safeGetResourceManager(bt branch.BranchType) (mgr rm.ResourceManager, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("resource manager not registered for branch type %v: %v", bt, r)
+		}
+	}()
+	mgr = rm.GetRmCacheInstance().GetResourceManager(bt)
+	return
 }
--- pkg/remoting/processor/client/rm_delete_undolog_processor_test.go
+++ pkg/remoting/processor/client/rm_delete_undolog_processor_test.go
@@ -1,240 +1,153 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package client
 
 import (
 	"context"
+	"database/sql"
+	"database/sql/driver"
 	"testing"
+	"time"
 
+	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/stretchr/testify/assert"
+	"seata.apache.org/seata-go/v2/pkg/datasource/sql/types"
+	"seata.apache.org/seata-go/v2/pkg/datasource/sql/undo"
 	"seata.apache.org/seata-go/v2/pkg/protocol/branch"
 	"seata.apache.org/seata-go/v2/pkg/protocol/message"
-	"seata.apache.org/seata-go/v2/pkg/remoting/getty"
 )
 
-func TestRmDeleteUndoLogProcessor_Process(t *testing.T) {
-	processor := &rmDeleteUndoLogProcessor{}
+func TestProcess_InvalidBodyType(t *testing.T) {
+	p := &rmDeleteUndoLogProcessor{}
+	err := p.Process(context.Background(), message.RpcMessage{
+		Body: "not a UndoLogDeleteRequest",
+	})
+	assert.Error(t, err)
+}
 
-	tests := []struct {
-		name       string
-		rpcMessage message.RpcMessage
-		wantErr    bool
-	}{
-		{
-			name: "process normal undo log delete request",
-			rpcMessage: message.RpcMessage{
-				ID:   1,
-				Type: message.GettyRequestTypeRequestSync,
-				Body: message.UndoLogDeleteRequest{
-					ResourceId: "jdbc:mysql://localhost:3306/seata",
-					SaveDays:   7,
-					BranchType: branch.BranchTypeAT,
-				},
+func TestProcess_NonATBranchType_Skipped(t *testing.T) {
+	p := &rmDeleteUndoLogProcessor{}
+	for _, bt := range []branch.BranchType{branch.BranchTypeXA, branch.BranchTypeTCC, branch.BranchTypeSAGA} {
+		err := p.Process(context.Background(), message.RpcMessage{
+			Body: message.UndoLogDeleteRequest{
+				ResourceId: "any-resource",
+				SaveDays:   7,
+				BranchType: bt,
 			},
-			wantErr: false,
-		},
-		{
-			name: "process undo log delete request with empty resource id",
-			rpcMessage: message.RpcMessage{
-				ID:   2,
-				Type: message.GettyRequestTypeRequestSync,
-				Body: message.UndoLogDeleteRequest{
-					ResourceId: "",
-					SaveDays:   10,
-					BranchType: branch.BranchTypeAT,
-				},
-			},
-			wantErr: false,
-		},
-		{
-			name: "process undo log delete request with zero save days",
-			rpcMessage: message.RpcMessage{
-				ID:   3,
-				Type: message.GettyRequestTypeRequestSync,
-				Body: message.UndoLogDeleteRequest{
-					ResourceId: "test-resource",
-					SaveDays:   0,
-					BranchType: branch.BranchTypeAT,
-				},
-			},
-			wantErr: false,
-		},
-		{
-			name: "process with oneway request type",
-			rpcMessage: message.RpcMessage{
-				ID:   4,
-				Type: message.GettyRequestTypeRequestOneway,
-				Body: message.UndoLogDeleteRequest{
-					ResourceId: "test-resource",
-					SaveDays:   7,
-					BranchType: branch.BranchTypeAT,
-				},
-			},
-			wantErr: false,
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			err := processor.Process(context.Background(), tt.rpcMessage)
-			if tt.wantErr {
-				assert.Error(t, err)
-			} else {
-				assert.NoError(t, err)
-			}
 		})
+		assert.NoError(t, err, "branch type %v should be skipped silently", bt)
 	}
 }
 
-func TestRmDeleteUndoLogProcessor_ProcessWithContext(t *testing.T) {
-	processor := &rmDeleteUndoLogProcessor{}
-
-	t.Run("background context", func(t *testing.T) {
-		err := processor.Process(context.Background(), message.RpcMessage{
-			ID:   1,
-			Type: message.GettyRequestTypeRequestSync,
-			Body: message.UndoLogDeleteRequest{
-				ResourceId: "test-resource-1",
-				SaveDays:   7,
-				BranchType: branch.BranchTypeAT,
-			},
-		})
-		assert.NoError(t, err)
-	})
-
-	t.Run("canceled context should be handled gracefully", func(t *testing.T) {
-		ctx, cancel := context.WithCancel(context.Background())
-		cancel() // cancel immediately
-		err := processor.Process(ctx, message.RpcMessage{
-			ID:   2,
-			Type: message.GettyRequestTypeRequestSync,
-			Body: message.UndoLogDeleteRequest{
-				ResourceId: "test-resource-2",
-				SaveDays:   7,
-				BranchType: branch.BranchTypeAT,
-			},
-		})
-		assert.NoError(t, err, "current impl does not use ctx; update when deletion logic is added")
-	})
-}
-
-func TestInitDeleteUndoLog(t *testing.T) {
-	getty.GetGettyClientHandlerInstance()
-
-	assert.NotPanics(t, func() {
-		initDeleteUndoLog()
-	}, "initDeleteUndoLog should not panic")
-}
-
-func TestRmDeleteUndoLogProcessor_Integration(t *testing.T) {
-	RegisterProcessor()
-
-	processor := &rmDeleteUndoLogProcessor{}
-
-	err := processor.Process(context.Background(), message.RpcMessage{
-		ID:    100,
-		Type:  message.GettyRequestTypeRequestSync,
-		Codec: 1,
+func TestProcess_ResourceNotFound_ReturnsNil(t *testing.T) {
+	p := &rmDeleteUndoLogProcessor{}
+	err := p.Process(context.Background(), message.RpcMessage{
 		Body: message.UndoLogDeleteRequest{
-			ResourceId: "jdbc:mysql://127.0.0.1:3306/seata_test",
+			ResourceId: "jdbc:mysql://not-registered:3306/db",
 			SaveDays:   7,
+			BranchType: branch.BranchTypeAT,
+		},
+	})
+	// GetResourceManager panic 和 resource 不在缓存，都会被 deleteExpiredUndoLog
+	// 的调用方吞掉记日志，Process 必须返回 nil
+	assert.NoError(t, err, "TC one-way: all internal errors must be swallowed")
+}
+
+func TestProcess_NegativeSaveDays_ReturnsNil(t *testing.T) {
+	p := &rmDeleteUndoLogProcessor{}
+	err := p.Process(context.Background(), message.RpcMessage{
+		Body: message.UndoLogDeleteRequest{
+			ResourceId: "jdbc:mysql://not-registered:3306/db",
+			SaveDays:   -32768,
 			BranchType: branch.BranchTypeAT,
 		},
 	})
 	assert.NoError(t, err)
 }
 
-func TestRmDeleteUndoLogProcessor_EdgeCases(t *testing.T) {
-	processor := &rmDeleteUndoLogProcessor{}
-
-	tests := []struct {
-		name       string
-		rpcMessage message.RpcMessage
-		wantErr    bool
-	}{
-		{
-			name: "max int16 save days",
-			rpcMessage: message.RpcMessage{
-				ID:   1,
-				Type: message.GettyRequestTypeRequestSync,
-				Body: message.UndoLogDeleteRequest{
-					ResourceId: "test-resource",
-					SaveDays:   32767,
-					BranchType: branch.BranchTypeAT,
-				},
-			},
-			wantErr: false,
-		},
-		{
-			name: "min int16 save days",
-			rpcMessage: message.RpcMessage{
-				ID:   2,
-				Type: message.GettyRequestTypeRequestSync,
-				Body: message.UndoLogDeleteRequest{
-					ResourceId: "test-resource",
-					SaveDays:   -32768,
-					BranchType: branch.BranchTypeAT,
-				},
-			},
-			wantErr: false,
-		},
-		{
-			name: "very long resource id",
-			rpcMessage: message.RpcMessage{
-				ID:   3,
-				Type: message.GettyRequestTypeRequestSync,
-				Body: message.UndoLogDeleteRequest{
-					ResourceId: "jdbc:mysql://very-long-hostname-that-could-be-used-in-production.example.com:3306/database_name_with_underscores",
-					SaveDays:   7,
-					BranchType: branch.BranchTypeAT,
-				},
-			},
-			wantErr: false,
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			err := processor.Process(context.Background(), tt.rpcMessage)
-			if tt.wantErr {
-				assert.Error(t, err)
-			} else {
-				assert.NoError(t, err)
-			}
-		})
-	}
+type mockDBResource struct {
+	db     *sql.DB
+	dbType types.DBType
 }
 
-func BenchmarkRmDeleteUndoLogProcessor_Process(b *testing.B) {
-	processor := &rmDeleteUndoLogProcessor{}
-	ctx := context.Background()
-	rpcMsg := message.RpcMessage{
-		ID:   1,
-		Type: message.GettyRequestTypeRequestSync,
+func (m *mockDBResource) GetDB() *sql.DB          { return m.db }
+func (m *mockDBResource) GetDbType() types.DBType { return m.dbType }
+
+func TestBatchDeleteByLogCreated_DeletesRows(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+
+	before := time.Now().AddDate(0, 0, -7)
+
+	mock.ExpectExec("DELETE FROM undo_log WHERE log_created <= ?").
+		WithArgs(sqlmock.AnyArg()).
+		WillReturnResult(sqlmock.NewResult(0, 1000))
+	mock.ExpectExec("DELETE FROM undo_log WHERE log_created <= ?").
+		WithArgs(sqlmock.AnyArg()).
+		WillReturnResult(sqlmock.NewResult(0, 50))
+
+	conn, err := db.Conn(context.Background())
+	assert.NoError(t, err)
+	defer conn.Close()
+
+	p := &rmDeleteUndoLogProcessor{}
+	err = p.batchDeleteByLogCreated(context.Background(), conn, before)
+	assert.NoError(t, err)
+	assert.NoError(t, mock.ExpectationsWereMet())
+}
+
+func TestBatchDeleteByLogCreated_CustomTableName(t *testing.T) {
+	original := undo.UndoConfig.LogTable
+	undo.UndoConfig.LogTable = "my_undo_log"
+	defer func() { undo.UndoConfig.LogTable = original }()
+
+	db, mock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+
+	mock.ExpectExec("DELETE FROM my_undo_log WHERE log_created <= ?").
+		WithArgs(sqlmock.AnyArg()).
+		WillReturnResult(sqlmock.NewResult(0, 0))
+
+	conn, err := db.Conn(context.Background())
+	assert.NoError(t, err)
+	defer conn.Close()
+
+	p := &rmDeleteUndoLogProcessor{}
+	err = p.batchDeleteByLogCreated(context.Background(), conn, time.Now())
+	assert.NoError(t, err)
+	assert.NoError(t, mock.ExpectationsWereMet())
+}
+
+func TestBatchDeleteByLogCreated_DBError(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+
+	mock.ExpectExec("DELETE FROM undo_log WHERE log_created <= ?").
+		WithArgs(sqlmock.AnyArg()).
+		WillReturnError(driver.ErrBadConn)
+
+	conn, err := db.Conn(context.Background())
+	assert.NoError(t, err)
+	defer conn.Close()
+
+	p := &rmDeleteUndoLogProcessor{}
+	err = p.batchDeleteByLogCreated(context.Background(), conn, time.Now())
+	assert.Error(t, err)
+}
+
+func BenchmarkProcess_NonAT(b *testing.B) {
+	p := &rmDeleteUndoLogProcessor{}
+	msg := message.RpcMessage{
 		Body: message.UndoLogDeleteRequest{
-			ResourceId: "jdbc:mysql://localhost:3306/seata",
+			ResourceId: "any",
 			SaveDays:   7,
-			BranchType: branch.BranchTypeAT,
+			BranchType: branch.BranchTypeXA,
 		},
 	}
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		_ = processor.Process(ctx, rpcMsg)
+		_ = p.Process(context.Background(), msg)
 	}
 }
